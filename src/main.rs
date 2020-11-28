92751813d2350e95166dc4e3ce3c1efab53272fd
Andreas Dzialocha <x12@adz.garden>
2020-11-28 22:14:20 +0100

    Move whole rpc server handling into server module

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,6 @@ extern crate serde;
 
 use async_ctrlc::CtrlC;
 use async_std::task;
-use std::net::{IpAddr, Ipv4Addr, SocketAddr};
 use structopt::StructOpt;
 
 mod config;
@@ -31,46 +30,7 @@ async fn main() -> std::io::Result<()> {
         config::Configuration::new(opt.data_dir).expect("Could not load configuration");
 
     let mut task_manager = tasks::TaskManager::new();
-
-    // Start HTTP RPC Server
-    let http_address = SocketAddr::new(
-        IpAddr::V4(Ipv4Addr::UNSPECIFIED),
-        configuration.server.http_port,
-    );
-    let http_server = server::start_http(
-        &http_address,
-        rpc::build_rpc_handler(),
-        configuration.server.http_threads,
-        configuration.server.max_payload,
-    )
-    .expect("Could not start HTTP server");
-    let http_close_handle = http_server.close_handle();
-
-    task_manager.spawn("HTTP RPC Server", async move {
-        info!("Start HTTP server at {}", http_address);
-        http_server.wait();
-        Ok(())
-    });
-
-    // Start WebSocket RPC Server
-    let ws_address = SocketAddr::new(
-        IpAddr::V4(Ipv4Addr::UNSPECIFIED),
-        configuration.server.ws_port,
-    );
-    let ws_server = server::start_ws(
-        &ws_address,
-        configuration.server.ws_max_connections,
-        configuration.server.max_payload,
-        rpc::build_rpc_handler(),
-    )
-    .expect("Could not start WebSocket server");
-    let ws_close_handle = ws_server.close_handle();
-
-    task_manager.spawn("WebSocket RPC Server", async move {
-        info!("Start WebSocket server at {}", ws_address);
-        ws_server.wait().unwrap();
-        Ok(())
-    });
+    let rpc_server = server::Server::new(configuration, &mut task_manager);
 
     // Run this until [CTRL] + [C] got pressed
     task::block_on(async {
@@ -78,9 +38,7 @@ async fn main() -> std::io::Result<()> {
         ctrlc.await;
     });
 
-    // Send close signals to RPC servers
-    http_close_handle.close();
-    ws_close_handle.close();
+    rpc_server.shutdown();
 
     // Wait until all tasks are gracefully shut down and exit
     task::block_on(task_manager.shutdown());
--- src/server.rs
+++ src/server.rs
@@ -1,14 +1,14 @@
 use jsonrpc_core::IoHandler;
 use std::io::{ErrorKind, Result};
-use std::net::SocketAddr;
+use std::net::{IpAddr, Ipv4Addr, SocketAddr};
 
 /// Type alias for http server.
-pub type HttpServer = http::Server;
+type HttpServer = http::Server;
 /// Type alias for ws server.
-pub type WebsocketServer = ws::Server;
+type WebsocketServer = ws::Server;
 
 /// Start HTTP RPC server listening on given address.
-pub fn start_http(
+fn start_http(
     addr: &SocketAddr,
     io: IoHandler,
     threads: usize,
@@ -21,7 +21,7 @@ pub fn start_http(
 }
 
 /// Start WebSocket RPC server listening on given address.
-pub fn start_ws(
+fn start_ws(
     addr: &SocketAddr,
     max_connections: usize,
     max_payload: usize,
@@ -34,9 +34,72 @@ pub fn start_ws(
         .map_err(|err| match err {
             ws::Error::Io(io) => io,
             ws::Error::ConnectionClosed => ErrorKind::BrokenPipe.into(),
-            e => {
-                error!("{}", e);
+            err => {
+                error!("{}", err);
                 ErrorKind::Other.into()
             }
         })
+}
+
+pub struct Server {
+    close_handle_http: http::CloseHandle,
+    close_handle_ws: ws::CloseHandle,
+}
+
+impl Server {
+    pub fn new(
+        configuration: super::config::Configuration,
+        task_manager: &mut super::tasks::TaskManager,
+    ) -> Self {
+        // Start HTTP RPC Server
+        let http_address = SocketAddr::new(
+            IpAddr::V4(Ipv4Addr::UNSPECIFIED),
+            configuration.server.http_port,
+        );
+        let http_server = start_http(
+            &http_address,
+            super::rpc::build_rpc_handler(),
+            configuration.server.http_threads,
+            configuration.server.max_payload,
+        )
+        .expect("Could not start HTTP server");
+        let close_handle_http = http_server.close_handle();
+
+        task_manager.spawn("HTTP RPC Server", async move {
+            info!("Start HTTP server at {}", http_address);
+            http_server.wait();
+            Ok(())
+        });
+
+        // Start WebSocket RPC Server
+        let ws_address = SocketAddr::new(
+            IpAddr::V4(Ipv4Addr::UNSPECIFIED),
+            configuration.server.ws_port,
+        );
+        let ws_server = start_ws(
+            &ws_address,
+            configuration.server.ws_max_connections,
+            configuration.server.max_payload,
+            super::rpc::build_rpc_handler(),
+        )
+        .expect("Could not start WebSocket server");
+        let close_handle_ws = ws_server.close_handle();
+
+        task_manager.spawn("WebSocket RPC Server", async move {
+            info!("Start WebSocket server at {}", ws_address);
+            ws_server.wait().unwrap();
+            Ok(())
+        });
+
+        Self {
+            close_handle_http,
+            close_handle_ws,
+        }
+    }
+
+    pub fn shutdown(self) {
+        // Send close signals to RPC servers
+        self.close_handle_http.close();
+        self.close_handle_ws.close();
+    }
 }
