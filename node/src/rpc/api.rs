b3ea023ca710cacb57004ac4a4ccb5f5ac1f177c
Andreas Dzialocha <x12@adz.garden>
2020-12-22 23:03:52 +0100

    Use BoxFuture for async response and Params for request parameters, add test

--- node/src/rpc/api.rs
+++ node/src/rpc/api.rs
@@ -1,12 +1,12 @@
-use jsonrpc_core::futures::future::{self, FutureResult};
-use jsonrpc_core::{Error, IoHandler, Result};
+use jsonrpc_core::futures::future;
+use jsonrpc_core::{BoxFuture, IoHandler, Params, Result};
 use jsonrpc_derive::rpc;
 use serde::{Deserialize, Serialize};
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Debug)]
 pub struct EntryArgsParams {
     author: String,
-    schema: String
+    schema: String,
 }
 
 #[derive(Serialize)]
@@ -15,20 +15,14 @@ pub struct EntryArgs {
     encoded_entry_backlink: String,
     encoded_entry_skiplink: String,
     last_seq_num: u64,
-    log_id: u64
+    log_id: u64,
 }
 
 /// Node RPC API methods.
 #[rpc(server)]
 pub trait Api {
-    #[rpc(name = "add")]
-    fn add(&self, a: u64, b: u64) -> Result<u64>;
-
-    #[rpc(name = "callAsync")]
-    fn call(&self, a: u64) -> FutureResult<String, Error>;
-
-    #[rpc(name = "panda_nextEntryArguments")]
-    fn next_entry_arguments(&self, params: EntryArgsParams) -> FutureResult<EntryArgs, Error>;
+    #[rpc(name = "panda_nextEntryArguments", params = "raw")]
+    fn next_entry_arguments(&self, params: Params) -> BoxFuture<Result<EntryArgs>>;
 }
 
 /// Service implementing API methods.
@@ -50,20 +44,79 @@ impl ApiService {
 }
 
 impl Api for ApiService {
-    fn add(&self, a: u64, b: u64) -> Result<u64> {
-        Ok(a + b)
+    fn next_entry_arguments(&self, params: Params) -> BoxFuture<Result<EntryArgs>> {
+        let _parsed: EntryArgsParams = params.parse().unwrap();
+
+        Box::pin(future::ready(
+            Ok(EntryArgs {
+                encoded_entry_backlink: String::from("encoded_entry_backlink"),
+                encoded_entry_skiplink: String::from("skiplink"),
+                last_seq_num: 1,
+                log_id: 0,
+            })
+            .into(),
+        ))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::ApiService;
+
+    // Helper method to generate valid JSON RPC request string
+    fn rpc_request(method: &str, params: &str) -> String {
+        format!(
+            r#"{{
+            "jsonrpc": "2.0",
+            "method": "{}",
+            "params": {},
+            "id": 1
+        }}"#,
+            method, params
+        )
+        .replace(" ", "")
+        .replace("\n", "")
     }
 
-    fn call(&self, _: u64) -> FutureResult<String, Error> {
-        future::ok("OK".to_owned()).into()
+    // Helper method to generate valid JSON RPC response string
+    fn rpc_response<'a>(result: &str) -> String {
+        format!(
+            r#"{{
+            "jsonrpc": "2.0",
+            "result": {},
+            "id": 1
+        }}"#,
+            result
+        )
+        .replace(" ", "")
+        .replace("\n", "")
     }
 
-    fn next_entry_arguments(&self, _params: EntryArgsParams) -> FutureResult<EntryArgs, Error> {
-        future::ok(EntryArgs { 
-            encoded_entry_backlink: String::from("encoded_entry_backlink"),
-            encoded_entry_skiplink: String::from("skiplink"),
-            last_seq_num: 1,
-            log_id: 0
-        }).into()
+    #[test]
+    fn next_entry_arguments() {
+        let io = ApiService::io_handler();
+
+        let request = rpc_request(
+            "panda_nextEntryArguments",
+            r#"
+            {
+                "author": "world",
+                "schema": "test"
+            }
+            "#,
+        );
+
+        let response = rpc_response(
+            r#"
+            {
+                "encodedEntryBacklink": "encoded_entry_backlink",
+                "encodedEntrySkiplink": "skiplink",
+                "lastSeqNum": 1,
+                "logId": 0
+            }
+            "#,
+        );
+
+        assert_eq!(io.handle_request_sync(&request), Some(response));
     }
 }
