b2baf406c1f634bee04df896c9e633132b032b8b
Andreas Dzialocha <x12@adz.garden>
2021-01-01 01:42:52 +0100

    Correct entry hash sizes in database

--- node/migrations/20201230002752_create-logs.sql
+++ node/migrations/20201230002752_create-logs.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS logs (
     author            VARCHAR(64)       NOT NULL,
     log_id            BIGINT            NOT NULL,
-    schema            VARCHAR(64)       NOT NULL,
+    schema            VARCHAR(128)      NOT NULL,
     PRIMARY KEY (author, schema)
 );
