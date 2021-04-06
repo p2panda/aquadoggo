4048645019a6b785f26eb368f2e7ea2e2237b06a
Andreas Dzialocha <x12@adz.garden>
2021-04-06 23:52:35 +0200

    Fix varchar size of hashes stored in SQL tables

--- aquadoggo/migrations/20201229215646_create-entries.sql
+++ aquadoggo/migrations/20201229215646_create-entries.sql
@@ -1,10 +1,10 @@
 CREATE TABLE IF NOT EXISTS entries (
     author            VARCHAR(64)       NOT NULL,
     entry_bytes       TEXT              NOT NULL,
-    entry_hash        VARCHAR(128)      NOT NULL UNIQUE,
+    entry_hash        VARCHAR(132)      NOT NULL UNIQUE,
     log_id            UNSIGNED BIGINT   NOT NULL,
     payload_bytes     TEXT,
-    payload_hash      VARCHAR(128)      NOT NULL,
+    payload_hash      VARCHAR(132)      NOT NULL,
     seq_num           UNSIGNED BIGINT   NOT NULL,
     PRIMARY KEY (author, log_id, seq_num)
 );
