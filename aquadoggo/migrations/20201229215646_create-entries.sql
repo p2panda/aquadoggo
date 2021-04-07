f612872e50f1b4e1c24472831449afa5ef873570
Andreas Dzialocha <x12@adz.garden>
2021-04-07 22:20:21 +0200

    Remove unknown type identifier for PostgreSQL

--- aquadoggo/migrations/20201229215646_create-entries.sql
+++ aquadoggo/migrations/20201229215646_create-entries.sql
@@ -2,9 +2,9 @@ CREATE TABLE IF NOT EXISTS entries (
     author            VARCHAR(64)       NOT NULL,
     entry_bytes       TEXT              NOT NULL,
     entry_hash        VARCHAR(128)      NOT NULL UNIQUE,
-    log_id            UNSIGNED BIGINT   NOT NULL,
+    log_id            BIGINT            NOT NULL,
     payload_bytes     TEXT,
     payload_hash      VARCHAR(128)      NOT NULL,
-    seq_num           UNSIGNED BIGINT   NOT NULL,
+    seq_num           BIGINT            NOT NULL,
     PRIMARY KEY (author, log_id, seq_num)
 );
