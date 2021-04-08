6ad3a28c17740ebd726c348c9d5c794528a602fd
Andreas Dzialocha <adzialocha@users.noreply.github.com>
2021-04-08 22:05:10 +0200

    Update 20201230002752_create-logs.sql

--- aquadoggo/migrations/20201230002752_create-logs.sql
+++ aquadoggo/migrations/20201230002752_create-logs.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS logs (
     author            VARCHAR(64)       NOT NULL,
-    log_id            UNSIGNED BIGINT   NOT NULL,
+    log_id            BIGINT   NOT NULL,
     schema            VARCHAR(132)      NOT NULL,
     PRIMARY KEY (author, schema, log_id)
 );
