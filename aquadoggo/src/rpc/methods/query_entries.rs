f5e24fa5584edc02fdc1a82990c1c256853d4113
sandreae <git@contact.samandreae.com>
2021-05-08 21:37:49 +0100

    Refactor tests for query_entries

--- aquadoggo/src/rpc/methods/query_entries.rs
+++ aquadoggo/src/rpc/methods/query_entries.rs
@@ -22,40 +22,54 @@ pub async fn query_entries(
     Ok(QueryEntriesResponse { entries })
 }
 
-// @TODO
-// #[cfg(test)]
-// mod tests {
-//     use crate::rpc::ApiService;
-//     use crate::test_helpers::{initialize_db, rpc_request, rpc_response};
-//     use p2panda_rs::atomic::Hash;
+#[cfg(test)]
+mod tests {
+    use tide_testing::TideTestingExt;
 
-//     #[async_std::test]
-//     async fn query_entries() {
-//         // Prepare test database
-//         let pool = initialize_db().await;
-//         let io = ApiService::io_handler(pool);
+    use crate::rpc::api::rpc_api_handler;
+    use crate::rpc::server::handle_http_request;
+    use crate::test_helpers::{initialize_db, rpc_request, rpc_response};
+    use p2panda_rs::atomic::Hash;
 
-//         let schema = Hash::new_from_bytes(vec![1, 2, 3]).unwrap();
+    #[async_std::test]
+    async fn query_entries() {
+        // Prepare test database
+        let pool = initialize_db().await;
+        let rpc_api_handler = rpc_api_handler(pool);
+       
+        // Create tider server with endpoints
+        let mut app = tide::with_state(rpc_api_handler);
+        app.at("/")
+            .get(|_| async { Ok("Used HTTP Method is not allowed. POST or OPTIONS is required") })
+            .post(handle_http_request);
+  
+        let schema = Hash::new_from_bytes(vec![1, 2, 3]).unwrap();
 
-//         // Prepare request to API
-//         let request = rpc_request(
-//             "panda_queryEntries",
-//             &format!(
-//                 r#"{{
-//                     "schema": "{}"
-//                 }}"#,
-//                 schema.as_hex(),
-//             ),
-//         );
+        // Prepare request to API
+        let request = rpc_request(
+            "panda_queryEntries",
+            &format!(
+                r#"{{
+                    "schema": "{}"
+                }}"#,
+                schema.as_hex(),
+            ),
+        );
 
-//         // Prepare expected response result
-//         let response = rpc_response(&format!(
-//             r#"{{
-//                 "entries": []
-//             }}"#,
-//         ));
+        // Prepare expected response result
+        let response = rpc_response(&format!(
+            r#"{{
+                "entries": []
+            }}"#,
+        ));
 
-//         // Send request to API and compare response with expected result
-//         assert_eq!(io.handle_request_sync(&request), Some(response));
-//     }
-// }
+        let response_body: serde_json::value::Value = app
+            .post("/")
+            .body(tide::Body::from_string(request.into()))
+            .content_type("application/json")
+            .recv_json()
+            .await.unwrap();
+
+        assert_eq!(response_body.to_string(), response);
+    }
+}
