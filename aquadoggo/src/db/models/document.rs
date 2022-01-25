8e19d406316eea8dcf9437dd794bb4f69d3ba205
Vincent Ahrend <mail@vincentahrend.com>
2022-01-25 13:39:54 +0100

    Fix errors constructing query string

--- aquadoggo/src/db/models/document.rs
+++ aquadoggo/src/db/models/document.rs
@@ -6,38 +6,36 @@ use sqlx::query;
 use crate::db::Pool;
 use crate::errors::Result;
 
-const FIELDS: [&str; 4] = ["document", "url", "title", "created"];
-
-fn build_insert_query(document_id: &Hash, instance: &Instance) -> String {
+fn build_insert_query(instance: &Instance) -> String {
     // The field spec enumerates all schema-specific vield names
-    let field_spec = FIELDS
+    let field_spec = instance
         .iter()
-        .map(|elem| elem.to_string())
+        .map(|(fieldname, _)| fieldname.clone())
         .reduce(|acc, val| format!("{}, {}", acc, val))
         .unwrap();
 
     // The parameter spec is a list of parameter placeholders with as many elements as there are
     // columns to insert
-    let parameter_spec = (0..FIELDS.len())
-        .map(|i| i.to_string())
-        .reduce(|elem, spec| format!("{}, ${}", spec, elem))
+    let parameter_spec = (0..instance.raw().len())
+        .map(|i| format!("${}", (i + 2)))
+        .reduce(|acc, elem| format!("{}, {}", acc, elem))
         .unwrap();
 
     format!(
         "
         INSERT INTO
             `bookmarks-0020c65567ae37efea293e34a9c7d13f8f2bf23dbdc3b5c7b9ab46293111c48fc78b`
-            ({})
+            (document, {})
         VALUES
-            ({})
+            ($1, {})
         ",
-        field_spec, parameter_spec
+        field_spec, parameter_spec,
     )
 }
 
 pub async fn write_document(pool: &Pool, document_id: &Hash, instance: &Instance) -> Result<()> {
     // Build query without any bound values
-    let query_string = build_insert_query(document_id, instance);
+    let query_string = build_insert_query(instance);
     let mut query = query(&query_string[..]);
 
     // Bind values for schema-independent columns
