ad281acb532c54036aea562cee17ac97918d0c82
Sam Andreae <contact@samandreae.com>
2022-01-27 20:16:29 +0000

    Handle log events with pretty_env_logger

--- aquadoggo/src/lib.rs
+++ aquadoggo/src/lib.rs
@@ -25,3 +25,23 @@ mod test_helpers;
 
 pub use config::Configuration;
 pub use runtime::Runtime;
+
+/// Init pretty_env_logger before the test suite runs to handle logging outputs.
+///
+/// Several of our dependencies (`sqlx`, `p2panda_rs`, `tide`) emmit log messages
+/// which we can handle and print using `pretty_env_logger`. Logging ehaviour
+/// can be customised at runtime. With eg. `RUST_LOG=p2panda_rs=info cargo t` or
+/// `RUST_LOG=openmls=debug cargo t`.
+///
+/// The `ctor` crate is used to define a global constructor function. This method
+/// will be run before any of the test suites.
+#[cfg(not(target_arch = "wasm32"))]
+#[cfg(test)]
+#[ctor::ctor]
+fn init() {
+    // If the `RUST_LOG` env var is not set skip initiation as we don't want
+    // to see any logs.
+    if std::env::var("RUST_LOG").is_ok() {
+        pretty_env_logger::init();
+    }
+}
