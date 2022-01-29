a60cc552b75fbdabc968981256058aeece9a7900
Andreas Dzialocha <x12@adz.garden>
2022-01-29 13:31:32 +0100

    Restructure code, parse hashes

--- aquadoggo/src/graphql/sql.rs
+++ aquadoggo/src/graphql/sql.rs
@@ -1,30 +1,40 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
-use apollo_parser::ast::{Argument as AstArgument, AstChildren, Definition, Document, Selection};
+use std::convert::TryInto;
+
+use apollo_parser::ast::{
+    Argument as AstArgument, AstChildren, Definition, Document, Selection, Value,
+};
 use apollo_parser::Parser;
+use p2panda_rs::hash::{Hash, HASH_SIZE};
 use sea_query::{Alias, Expr, Query};
 
 use crate::errors::Result;
 
-#[derive(Debug)]
-struct Root {
-    /// Schema hash of the queried document view.
-    schema: String,
+const MAX_SCHEMA_NAME: usize = 64;
+const SCHEMA_NAME_SEPARATOR: &str = "_";
 
-    /// Filter or sorting arguments which can be applied to the results.
-    arguments: Option<Vec<Argument>>,
+fn parse_graphql_query(query: &str) -> Result<Document> {
+    let parser = Parser::new(query);
+    let ast = parser.parse();
 
-    /// Fields which give general information on the given document view.
-    meta_fields: Option<Vec<MetaField>>,
+    if ast.errors().len() > 0 {
+        panic!("Parsing failed");
+    }
 
-    /// Materialized field values of the document view.
-    fields: Option<Vec<Field>>,
+    let document = ast.document();
+
+    Ok(document)
 }
+
+type MetaFields = Option<Vec<MetaField>>;
+type Fields = Option<Vec<Field>>;
+type Arguments = Option<Vec<Argument>>;
 
 #[derive(Debug)]
 enum Argument {
     /// Filter document views by document hash.
-    DocumentHash(String),
+    DocumentHash(Hash),
 }
 
 #[derive(Debug)]
@@ -45,7 +55,7 @@ enum Field {
 #[derive(Debug)]
 struct Relation {
     /// Schema hash of the related document view.
-    schema: String,
+    schema: Schema,
 
     /// Fields which give general information on the related document view.
     meta_fields: Option<Vec<MetaField>>,
@@ -54,166 +64,214 @@ struct Relation {
     fields: Option<Vec<Field>>,
 }
 
-fn parse_graphql_query(query: &str) -> Result<Document> {
-    let parser = Parser::new(query);
-    let ast = parser.parse();
+#[derive(Debug)]
+struct Schema {
+    /// Given name of this schema.
+    name: String,
 
-    if ast.errors().len() > 0 {
-        panic!("Parsing failed");
-    }
-
-    let document = ast.document();
-
-    Ok(document)
+    /// Hash of the schema.
+    hash: Hash,
 }
 
-fn get_fields(selection_set: AstChildren<Selection>) -> Result<Vec<Field>> {
-    let mut fields = Vec::<Field>::new();
+impl Schema {
+    pub fn parse(str: &str) -> Result<Self> {
+        if str.len() > MAX_SCHEMA_NAME + SCHEMA_NAME_SEPARATOR.len() + (HASH_SIZE * 2) {
+            panic!("Too long");
+        }
 
-    for selection in selection_set {
-        match selection {
-            Selection::Field(field) => {
-                let name = field.name().expect("Needs a name").text().to_string();
+        let mut separated = str.split(SCHEMA_NAME_SEPARATOR);
 
-                match field.selection_set() {
-                    Some(set) => {
-                        let (meta_fields, relation_fields) =
-                            get_meta_fields(set.selections()).unwrap();
+        let name = separated.next().expect("Could not parse name").into();
 
-                        fields.push(Field::Relation(Relation {
-                            schema: name,
-                            meta_fields,
-                            fields: relation_fields,
-                        }));
-                    }
-                    None => {
-                        fields.push(Field::Value(name));
-                    }
-                };
-            }
-            _ => panic!("Needs a field"),
-        };
+        let hash = separated
+            .next()
+            .expect("Could not parse hash")
+            .try_into()
+            .expect("Invalid hash");
+
+        Ok(Self { name, hash })
     }
 
-    Ok(fields)
+    pub fn table_name(&self) -> String {
+        format!(
+            "{}{}{}",
+            self.name,
+            SCHEMA_NAME_SEPARATOR,
+            self.hash.as_str()
+        )
+    }
 }
 
-fn get_meta_fields(
-    selection_set: AstChildren<Selection>,
-) -> Result<(Option<Vec<MetaField>>, Option<Vec<Field>>)> {
-    let mut meta = Vec::<MetaField>::new();
-    let mut fields = None;
+#[derive(Debug)]
+struct AbstractQuery {
+    /// Schema hash of the queried document view.
+    schema: Schema,
 
-    for selection in selection_set {
-        match selection {
-            Selection::Field(field) => {
-                let name = field.name().expect("Needs a name").text().to_string();
+    /// Filter or sorting arguments which can be applied to the results.
+    arguments: Arguments,
 
-                match name.as_str() {
-                    "fields" => {
-                        let set = field.selection_set().expect("Needs selection set");
-                        fields = Some(get_fields(set.selections()).unwrap());
-                    }
-                    "document" => {
-                        meta.push(MetaField::DocumentHash);
-                    }
-                    _ => panic!("Unknown meta field"),
-                };
-            }
-            _ => panic!("Needs a field"),
-        };
+    /// Fields which give general information on the given document view.
+    meta_fields: MetaFields,
+
+    /// Materialized field values of the document view.
+    fields: Fields,
+}
+
+impl AbstractQuery {
+    fn get_fields(selection_set: AstChildren<Selection>) -> Result<Vec<Field>> {
+        let mut fields = Vec::<Field>::new();
+
+        for selection in selection_set {
+            match selection {
+                Selection::Field(field) => {
+                    let field_name = field.name().expect("Needs a name").text().to_string();
+
+                    match field.selection_set() {
+                        Some(set) => {
+                            let (meta_fields, relation_fields) =
+                                Self::get_meta_fields(set.selections()).unwrap();
+
+                            fields.push(Field::Relation(Relation {
+                                schema: Schema::parse(&field_name).expect("Invalid schema"),
+                                meta_fields,
+                                fields: relation_fields,
+                            }));
+                        }
+                        None => {
+                            fields.push(Field::Value(field_name));
+                        }
+                    };
+                }
+                _ => panic!("Needs a field"),
+            };
+        }
+
+        Ok(fields)
     }
 
-    let meta_option = if meta.is_empty() { None } else { Some(meta) };
+    fn get_meta_fields(selection_set: AstChildren<Selection>) -> Result<(MetaFields, Fields)> {
+        let mut meta = Vec::<MetaField>::new();
+        let mut fields = None;
 
-    Ok((meta_option, fields))
-}
+        for selection in selection_set {
+            match selection {
+                Selection::Field(field) => {
+                    let name = field.name().expect("Needs a name").text().to_string();
 
-fn get_root(document: Document) -> Result<Root> {
-    let definition = document
-        .definitions()
-        .nth(0)
-        .expect("Needs to contain at least one definition");
+                    match name.as_str() {
+                        "fields" => {
+                            let set = field.selection_set().expect("Needs selection set");
+                            fields = Some(Self::get_fields(set.selections()).unwrap());
+                        }
+                        "document" => {
+                            meta.push(MetaField::DocumentHash);
+                        }
+                        _ => panic!("Unknown meta field"),
+                    };
+                }
+                _ => panic!("Needs a field"),
+            };
+        }
 
-    if let Definition::OperationDefinition(op_def) = definition {
-        let selection = op_def
-            .selection_set()
-            .expect("Needs to have a selection set")
-            .selections()
-            .nth(0)
-            .expect("Needs to have one selection");
+        let meta_option = if meta.is_empty() { None } else { Some(meta) };
 
-        if let Selection::Field(field) = selection {
-            let schema = field
-                .name()
-                .expect("Needs to have a name")
-                .text()
-                .to_string();
+        Ok((meta_option, fields))
+    }
 
-            let arguments = match field.arguments() {
-                Some(args) => Some(get_arguments(args.arguments()).unwrap()),
-                None => None,
+    fn get_arguments(args: AstChildren<AstArgument>) -> Result<Vec<Argument>> {
+        let mut arguments = Vec::<Argument>::new();
+
+        for arg in args {
+            let name = arg.name().expect("Needs a name").to_string();
+            let value = arg.value().expect("Needs a value");
+
+            let arg_value = match name.as_str() {
+                "document" => match value {
+                    Value::StringValue(str_value) => {
+                        let str: String = str_value.into();
+                        let hash = str.try_into().expect("Invalid hash");
+                        Argument::DocumentHash(hash)
+                    }
+                    _ => {
+                        panic!("Expected string");
+                    }
+                },
+                _ => panic!("Unknown argument"),
             };
 
-            let (meta_fields, fields) = get_meta_fields(
-                field
+            arguments.push(arg_value);
+        }
+
+        Ok(arguments)
+    }
+
+    pub fn new_from_query(query: &str) -> Result<Self> {
+        let document = parse_graphql_query(query).unwrap();
+        let root = Self::new_from_document(document).unwrap();
+        Ok(root)
+    }
+
+    pub fn new_from_document(document: Document) -> Result<Self> {
+        let definition = document
+            .definitions()
+            .next()
+            .expect("Needs to contain at least one definition");
+
+        if let Definition::OperationDefinition(op_def) = definition {
+            let selection = op_def
+                .selection_set()
+                .expect("Needs to have a selection set")
+                .selections()
+                .next()
+                .expect("Needs to have one selection");
+
+            if let Selection::Field(field) = selection {
+                let schema = field
+                    .name()
+                    .expect("Needs to have a name")
+                    .text()
+                    .to_string();
+
+                let arguments = field
+                    .arguments()
+                    .map(|args| Self::get_arguments(args.arguments()).unwrap());
+
+                let selections = field
                     .selection_set()
                     .expect("Needs to have a selection set")
-                    .selections(),
-            )
-            .unwrap();
+                    .selections();
 
-            return Ok(Root {
-                schema,
-                meta_fields,
-                arguments,
-                fields,
-            });
+                let (meta_fields, fields) = Self::get_meta_fields(selections).unwrap();
+
+                let schema = Schema::parse(&schema).expect("Invalid schema");
+
+                Ok(Self {
+                    schema,
+                    meta_fields,
+                    arguments,
+                    fields,
+                })
+            } else {
+                panic!("Needs to be a field");
+            }
         } else {
-            panic!("Needs to be a field");
+            panic!("Needs to be an operation definition");
         }
-    } else {
-        panic!("Needs to be an operation definition");
     }
 }
 
-fn get_arguments(args: AstChildren<AstArgument>) -> Result<Vec<Argument>> {
-    let mut arguments = Vec::<Argument>::new();
-
-    for arg in args {
-        let name = arg.name().expect("Needs a name").to_string();
-        let value = arg.value().expect("Needs a value").to_string();
-
-        let arg_value = match name.as_str() {
-            "document" => Argument::DocumentHash(value),
-            _ => panic!("Unknown argument"),
-        };
-
-        arguments.push(arg_value);
-    }
-
-    Ok(arguments)
-}
-
-fn parse(query: &str) -> Result<Root> {
-    let document = parse_graphql_query(query).unwrap();
-    let root = get_root(document).unwrap();
-    Ok(root)
-}
-
-fn validate(root: &Root, schema: u64) -> Result<()> {
-    Ok(())
-}
-
-fn root_to_sql(root: Root) -> Result<String> {
+fn root_to_sql(root: AbstractQuery) -> Result<String> {
     let mut query = Query::select();
 
     if root.fields.is_some() {
         for field in root.fields.unwrap() {
-            if let Field::Value(name) = field {
-                query.column(Alias::new(&name));
-            } else {
-                panic!("Relations not supported yet");
+            match field {
+                Field::Value(name) => {
+                    query.column(Alias::new(&name));
+                }
+                Field::Relation(_) => {
+                    panic!("Relations not supported yet");
+                }
             }
         }
     }
@@ -228,32 +286,26 @@ fn root_to_sql(root: Root) -> Result<String> {
         }
     }
 
+    query.from(Alias::new(&root.schema.table_name()));
+
     if root.arguments.is_some() {
         for arg in root.arguments.unwrap() {
             match arg {
                 Argument::DocumentHash(value) => {
-                    query.and_where(Expr::col(Alias::new("document")).eq(value));
+                    query.and_where(Expr::col(Alias::new("document")).eq(value.as_str()));
                 }
             }
         }
     }
 
-    let sql = query
-        .from(Alias::new(&root.schema))
-        .to_string(sea_query::PostgresQueryBuilder);
+    let sql = query.to_string(sea_query::PostgresQueryBuilder);
 
     Ok(sql)
 }
 
 pub fn gql_to_sql(query: &str) -> Result<String> {
-    // @TODO: We will pass in the schema later ..
-    let schema = 123;
-
     // Parse GraphQL to our own abstract query representation
-    let root = parse(query).unwrap();
-
-    // Validate query with application schema
-    validate(&root, schema).unwrap();
+    let root = AbstractQuery::new_from_query(query).unwrap();
 
     // Convert to SQL query
     let sql = root_to_sql(root).unwrap();
@@ -268,7 +320,7 @@ mod tests {
     #[test]
     fn parser() {
         let query = "{
-            festivalEvents(document: ABC123) {
+            festivalevents_0020c65567ae37efea293e34a9c7d13f8f2bf23dbdc3b5c7b9ab46293111c48fc78b(document: \"0020b177ec1bf26dfb3b7010d473e6d44713b29b765b99c6e60ecbfae742de496543\") {
                 document
                 fields {
                     title
@@ -281,7 +333,7 @@ mod tests {
 
         assert_eq!(
             sql,
-            "SELECT \"title\", \"description\", \"document\" FROM \"festivalEvents\" WHERE \"document\" = 'ABC123'"
+            "SELECT \"title\", \"description\", \"document\" FROM \"festivalevents_0020c65567ae37efea293e34a9c7d13f8f2bf23dbdc3b5c7b9ab46293111c48fc78b\" WHERE \"document\" = '0020b177ec1bf26dfb3b7010d473e6d44713b29b765b99c6e60ecbfae742de496543'"
         );
     }
 }
