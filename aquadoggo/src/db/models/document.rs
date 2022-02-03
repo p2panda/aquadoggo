20d4315257bd13335e5b7db0fc9686fadba8c4fa
Sam Andreae <contact@samandreae.com>
2022-02-03 17:46:27 +0000

    Update p2panda-rs

--- aquadoggo/src/db/models/document.rs
+++ aquadoggo/src/db/models/document.rs
@@ -1,5 +1,5 @@
+use p2panda_rs::document::DocumentView;
 use p2panda_rs::hash::Hash;
-use p2panda_rs::instance::Instance;
 use p2panda_rs::operation::OperationValue;
 use sqlx::{query, query_as, FromRow};
 
@@ -14,9 +14,9 @@ pub struct Bookmark {
     pub title: String,
 }
 
-fn build_insert_query(instance: &Instance) -> String {
+fn build_insert_query(document_view: &DocumentView) -> String {
     // The field spec enumerates all schema-specific vield names
-    let field_spec = instance
+    let field_spec = document_view
         .keys()
         .into_iter()
         .reduce(|acc, val| format!("{}, {}", acc, val))
@@ -24,14 +24,14 @@ fn build_insert_query(instance: &Instance) -> String {
 
     // The parameter spec is a list of parameter placeholders with as many elements as there are
     // columns to insert
-    let parameter_spec = (0..instance.len())
+    let parameter_spec = (0..document_view.len())
         // Add 2 because paramas are 1-indexed and "document" is
         // a static parameter inserted before these dynamic ones
         .map(|i| format!("${}", (i + 2)))
         .reduce(|acc, elem| format!("{}, {}", acc, elem))
         .unwrap();
 
-    let update_spec = instance
+    let update_spec = document_view
         .iter()
         .enumerate()
         // Make key-binding pairs like "key=$2"
@@ -72,16 +72,20 @@ pub async fn get_bookmarks(pool: &Pool) -> Result<Vec<Bookmark>> {
     Ok(bookmarks)
 }
 
-pub async fn write_document(pool: &Pool, document_id: &Hash, instance: &Instance) -> Result<()> {
+pub async fn write_document(
+    pool: &Pool,
+    document_id: &Hash,
+    document_view: &DocumentView,
+) -> Result<()> {
     // Build query without any bound values
-    let query_string = build_insert_query(instance);
+    let query_string = build_insert_query(document_view);
     let mut query = query(&query_string[..]);
 
     // Bind values for schema-independent columns
     query = query.bind(document_id.as_str());
 
     // Bind values for schema-specific columns
-    for (key, value) in instance.iter() {
+    for (key, value) in document_view.iter() {
         let string_value = match value {
             // OperationValue::Boolean(value) => value,
             // OperationValue::Integer(value) => value,
