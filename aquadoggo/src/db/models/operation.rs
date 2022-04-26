9a578ad2f92ecfedb6960bc0ac5d7040962af112
Sam Andreae <contact@samandreae.com>
2022-04-26 15:03:05 +0900

    SQL queries for inserting an operation

--- aquadoggo/src/db/models/operation.rs
+++ aquadoggo/src/db/models/operation.rs
@@ -1,19 +1,26 @@
-use async_trait::async_trait;
 // SPDX-License-Identifier: AGPL-3.0-or-later
+use async_trait::async_trait;
+use futures::future::try_join_all;
+use serde::{Serialize, Serializer};
+use sqlx::query;
+
 use p2panda_rs::document::{DocumentId, DocumentViewId};
 use p2panda_rs::hash::Hash;
 use p2panda_rs::identity::Author;
-use p2panda_rs::operation::{OperationFields, OperationId};
-use p2panda_rs::schema::SchemaId;
+use p2panda_rs::operation::{OperationAction, OperationFields, OperationId};
+use p2panda_rs::schema::{FieldType, SchemaId};
 use p2panda_rs::Validate;
+
+use crate::db::store::SqlStorage;
 
 /////// DB ROWS ///////
 // Structs representing data as it is stored in rows in the db
 pub struct OperationRow {
     id: String,
     author: String,
+    action: String,
     entry_hash: String,
-    schema_id_hash: String,
+    schema_id_short: String,
 }
 
 pub struct PreviousOperationRelationRow {
@@ -31,6 +38,7 @@ pub struct OperationFieldRow {
 }
 
 type DocumentViewIdHash = Hash;
+type SchemaIdShort = String;
 
 ////// OPERATION STORAGE TRAITS ///////
 
@@ -38,66 +46,218 @@ pub trait AsStorageOperation: Sized + Clone + Send + Sync + Validate {
     /// The error type returned by this traits' methods.
     type AsStorageOperationError: 'static + std::error::Error;
 
+    fn action(&self) -> OperationAction;
+
     fn author(&self) -> Author;
-
-    fn id(&self) -> OperationId;
-
-    fn schema_id(&self) -> SchemaId;
-
-    fn fields(&self) -> OperationFields;
-
-    fn previous_operations(&self) -> Vec<OperationId>;
 
     fn document_id(&self) -> DocumentId;
 
-    // We could store relations between document view id hashes
-    // and the operation id graph tips in order to reconstruct the
-    // complete DocumentViewId if it's needed (is it needed?).
-    fn document_view_id(&self) -> DocumentViewId;
+    // // and the operation id graph tips in order to reconstruct the
+    // // complete DocumentViewId if it's needed (is it needed?).
+    // // We could store relations between document view id hashes
+    // fn document_view_id(&self) -> DocumentViewId;
 
     fn document_view_id_hash(&self) -> DocumentViewIdHash;
+
+    fn fields(&self) -> OperationFields;
+
+    fn id(&self) -> OperationId;
+
+    fn previous_operations(&self) -> Vec<OperationId>;
+
+    fn schema_id(&self) -> SchemaId;
+
+    fn schema_id_short(&self) -> SchemaIdShort;
 }
 
-pub struct OperationStoreError {}
+/// `LogStorage` errors.
+#[derive(thiserror::Error, Debug)]
+pub enum OperationStorageError {
+    /// Catch all error which implementers can use for passing their own errors up the chain.
+    #[error("Ahhhhh!!!!")]
+    Error,
+}
 
 #[async_trait]
 pub trait OperationStore<StorageOperation: AsStorageOperation> {
     async fn insert_operation(
         &self,
         operation: &StorageOperation,
-    ) -> Result<bool, OperationStoreError>;
+    ) -> Result<bool, OperationStorageError>;
 
-    async fn insert_previous_operation_relation(
-        &self,
-        child: &OperationId,
-        parent: &OperationId,
-    ) -> Result<bool, OperationStoreError>;
+    // async fn get_operation(
+    //     &self,
+    //     id: OperationId,
+    // ) -> Result<StorageOperation, OperationStorageError>;
+}
 
-    async fn insert_operation_fields(
-        &self,
-        fields: &OperationFields,
-        document_relation: &DocumentId,
-        document_view_relation: &DocumentViewIdHash,
-    ) -> Result<bool, OperationStoreError>;
+////// IMPLEMENT THE STORAGE TRAITS //////
 
-    // Helper method for inserting all operation relation dependencies
-    async fn insert_operation_relations(
-        &self,
-        operation: &StorageOperation,
-    ) -> Result<bool, OperationStoreError> {
-        for previous_operation in operation.previous_operations() {
-            self.insert_previous_operation_relation(&operation.id(), &previous_operation)
-                .await?;
-        }
-        self.insert_operation_fields(
-            &operation.fields(),
-            &operation.document_id(),
-            &operation.document_view_id_hash(),
-        )
-        .await?;
-        Ok(true)
+#[derive(Debug, Clone)]
+pub struct DoggoOperation {
+    action: OperationAction,
+    author: Author,
+    document_id: DocumentId,
+    document_view_id_hash: DocumentViewIdHash,
+    fields: OperationFields,
+    id: OperationId,
+    previous_operations: Vec<OperationId>,
+    schema_id: SchemaId,
+}
+
+impl Validate for DoggoOperation {
+    type Error = OperationStorageError;
+
+    fn validate(&self) -> Result<(), Self::Error> {
+        Ok(())
+    }
+}
+
+impl AsStorageOperation for DoggoOperation {
+    type AsStorageOperationError = OperationStorageError;
+
+    fn action(&self) -> OperationAction {
+        self.action.clone()
+    }
+    fn author(&self) -> Author {
+        self.author.clone()
     }
 
-    async fn get_operation(&self, id: OperationId)
-        -> Result<StorageOperation, OperationStoreError>;
+    fn id(&self) -> OperationId {
+        self.id.clone()
+    }
+
+    fn schema_id_short(&self) -> SchemaIdShort {
+        match &self.schema_id {
+            SchemaId::Application(name, document_view_id) => {
+                format!("{}__{}", name, document_view_id.hash().as_str())
+            }
+            _ => self.schema_id.as_str(),
+        }
+    }
+
+    fn schema_id(&self) -> SchemaId {
+        self.schema_id.clone()
+    }
+
+    fn fields(&self) -> OperationFields {
+        self.fields.clone()
+    }
+
+    fn previous_operations(&self) -> Vec<OperationId> {
+        self.previous_operations.clone()
+    }
+
+    fn document_id(&self) -> DocumentId {
+        self.document_id.clone()
+    }
+
+    fn document_view_id_hash(&self) -> DocumentViewIdHash {
+        self.document_view_id_hash.clone()
+    }
+}
+
+#[async_trait]
+impl OperationStore<DoggoOperation> for SqlStorage {
+    async fn insert_operation(
+        &self,
+        operation: &DoggoOperation,
+    ) -> Result<bool, OperationStorageError> {
+        // We need `as_str()` for OperationAction
+        let action = match operation.action() {
+            OperationAction::Create => "create",
+            OperationAction::Update => "update",
+            OperationAction::Delete => "delete",
+        };
+        let operation_inserted = query(
+            "
+            INSERT INTO
+                operations_v1 (
+                    author,
+                    operation_id,
+                    entry_hash,
+                    action,
+                    schema_id_short,
+                )
+            VALUES
+                ($1, $2, $3, $4, $5)
+            ",
+        )
+        .bind(operation.author().as_str())
+        .bind(operation.id().as_str())
+        .bind(operation.id().as_hash().as_str())
+        .bind(action)
+        .bind(operation.schema_id_short().as_str())
+        .execute(&self.pool)
+        .await
+        .map_err(|e| OperationStorageError::Error)?
+        .rows_affected()
+            == 1;
+
+        let previous_operations_inserted =
+            try_join_all(operation.previous_operations().iter().map(|prev_op_id| {
+                query(
+                    "
+            INSERT INTO
+                previous_operations_v1 (
+                    parent_operation_id,
+                    child_operation_id,
+                 )
+            VALUES
+                ($1, $2)
+            ",
+                )
+                .bind(prev_op_id.as_str())
+                .bind(operation.id().as_str().to_owned())
+                .execute(&self.pool)
+            }))
+            .await
+            .map_err(|e| OperationStorageError::Error)? // Coerce error here
+            .iter()
+            .try_for_each(|result| {
+                if result.rows_affected() == 1 {
+                    Ok(())
+                } else {
+                    Err(OperationStorageError::Error)
+                }
+            })
+            .is_ok();
+
+        let fields_inserted = try_join_all(operation.fields().iter().map(|field| {
+            query(
+                "
+                INSERT INTO
+                    operation_fields_v1 (
+                        operation_id,
+                        name,
+                        field_type,
+                        value,
+                        relation_document_id,
+                        relation_document_view_id_hash,
+                    )
+                VALUES
+                    ($1, $2, $3, $4, $5)
+            ",
+            )
+            .bind(operation.id().as_str().to_owned())
+            .bind(field.0.to_owned())
+            .bind(field.1.field_type())
+            .bind(operation.document_id().as_str().to_owned())
+            .bind(operation.document_view_id_hash().as_str().to_owned())
+            .execute(&self.pool)
+        }))
+        .await
+        .map_err(|e| OperationStorageError::Error)? // Coerce error here
+        .iter()
+        .try_for_each(|result| {
+            if result.rows_affected() == 1 {
+                Ok(())
+            } else {
+                Err(OperationStorageError::Error)
+            }
+        })
+        .is_ok();
+
+        Ok(operation_inserted && previous_operations_inserted && fields_inserted)
+    }
 }
