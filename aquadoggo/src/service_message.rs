95cb7fe12854ee0b89dcee2be921a54a7410f993
Andreas Dzialocha <x12@adz.garden>
2022-04-28 15:55:07 +0200

    Add documentation for ServiceMessage

--- aquadoggo/src/service_message.rs
+++ aquadoggo/src/service_message.rs
@@ -2,7 +2,11 @@
 
 use crate::service_manager::Sender;
 
+/// Sender for cross-service communication bus.
 pub type ServiceSender = Sender<ServiceMessage>;
 
+/// Messages which can be sent on the communication bus.
 #[derive(Clone)]
-pub enum ServiceMessage {}
+pub enum ServiceMessage {
+    // Nothing here to see yet ..!
+}
