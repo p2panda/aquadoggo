6e25f28ae549ec4008c02ca5b07c249ccd909487
Sam Andreae <contact@samandreae.com>
2022-05-02 16:56:59 +0900

    Comments and clean-up

--- aquadoggo/src/db/models/document_view.rs
+++ aquadoggo/src/db/models/document_view.rs
@@ -15,17 +15,18 @@ use p2panda_rs::Validate;
 
 use crate::db::store::SqlStorage;
 
+/// The string name of a documents field
 type FieldName = String;
-type DocumentViewFields = BTreeMap<FieldName, OperationValue>;
+
+/// A map associating fields identified by their name with an operation which
+/// conatins this fields value(s).
 type FieldIds = BTreeMap<FieldName, OperationId>;
 
-#[derive(FromRow, Debug)]
-pub struct DocumentViewRow {
-    document_view_id_hash: String,
+/// The fields of a document view.
+type DocumentViewFields = BTreeMap<FieldName, OperationValue>;
 
-    schema_id_short: String,
-}
-
+/// A row in the DB which expresses a document view field. If the field type
+/// is a relation list, then a record for each item in the list will be recorded
 #[derive(FromRow, Debug)]
 pub struct DocumentViewFieldRow {
     name: String,
@@ -35,13 +36,7 @@ pub struct DocumentViewFieldRow {
     value: String,
 }
 
-#[derive(Debug, Clone)]
-pub struct DoggoDocumentView {
-    document_view: DocumentView,
-    field_ids: FieldIds,
-    schema_id: SchemaId,
-}
-
+/// WIP: Storage trait representing a document view.
 pub trait AsStorageDocumentView: Sized + Clone + Send + Sync + Validate {
     /// The error type returned by this traits' methods.
     type AsStorageDocumentViewError: 'static + std::error::Error;
@@ -51,6 +46,14 @@ pub trait AsStorageDocumentView: Sized + Clone + Send + Sync + Validate {
     fn get(&self, key: &str) -> Option<&OperationValue>;
     fn schema_id(&self) -> SchemaId;
     fn field_ids(&self) -> FieldIds;
+}
+
+/// WIP: Aquadoggo struct which will implement the above trait
+#[derive(Debug, Clone)]
+pub struct DoggoDocumentView {
+    document_view: DocumentView,
+    field_ids: FieldIds,
+    schema_id: SchemaId,
 }
 
 impl DoggoDocumentView {
@@ -63,14 +66,7 @@ impl DoggoDocumentView {
     }
 }
 
-impl Validate for DoggoDocumentView {
-    type Error = DocumentViewStorageError;
-
-    fn validate(&self) -> Result<(), Self::Error> {
-        Ok(())
-    }
-}
-
+/// WIP: Aquadoggo implementation of the above trait
 impl AsStorageDocumentView for DoggoDocumentView {
     type AsStorageDocumentViewError = DocumentViewStorageError;
 
@@ -91,6 +87,14 @@ impl AsStorageDocumentView for DoggoDocumentView {
     }
 }
 
+impl Validate for DoggoDocumentView {
+    type Error = DocumentViewStorageError;
+
+    fn validate(&self) -> Result<(), Self::Error> {
+        Ok(()) // lolz
+    }
+}
+
 /// `DocumentStore` errors.
 #[derive(thiserror::Error, Debug)]
 pub enum DocumentViewStorageError {
@@ -99,6 +103,7 @@ pub enum DocumentViewStorageError {
     Custom(String),
 }
 
+/// Storage traits for documents and document views.
 #[async_trait]
 pub trait DocumentStore<StorageDocumentView: AsStorageDocumentView> {
     async fn insert_document_view(
@@ -116,16 +121,22 @@ pub trait DocumentStore<StorageDocumentView: AsStorageDocumentView> {
 
 #[async_trait]
 impl DocumentStore<DoggoDocumentView> for SqlStorage {
+    /// Insert a document_view into the db. Requires that all relevent operations
+    /// are already in the db as this method only creates relations between
+    /// document view fields and their current values (last updated operation value).
+    ///
+    /// QUESTION: Is this too implementation specific? It assumes quite a lot about the db
+    /// structure and others may wish to structure things differently.
     async fn insert_document_view(
         &self,
         document_view_id: &DocumentViewId,
         field_ids: &FieldIds,
         schema_id: &SchemaId,
     ) -> Result<bool, DocumentViewStorageError> {
-        // Observations:
+        // OBSERVATIONS:
         // - we need to know which operation was LWW for each field.
         // - this is different from knowing the document view id, which is
-        // just the tip(s) of the graph, and will likely now contain a value
+        // just the tip(s) of the graph, and will likely not contain a value
         // for every field.
         // - we could record the operation id for each value when we build the
         //   document
@@ -136,6 +147,8 @@ impl DocumentStore<DoggoDocumentView> for SqlStorage {
         // - we could also pass in a full list of already sorted operations,
         // these are already stored on `Document` so could be re-used from
         // there.
+
+        // Insert document view field relations into the db
         let field_relations_inserted = try_join_all(field_ids.iter().map(|field| {
             query(
                 "
@@ -169,6 +182,7 @@ impl DocumentStore<DoggoDocumentView> for SqlStorage {
         })
         .is_ok();
 
+        // Construct short schema id
         let schema_id_short = match &schema_id {
             SchemaId::Application(name, document_view_id) => {
                 format!("{}__{}", name, document_view_id.hash().as_str())
@@ -176,7 +190,8 @@ impl DocumentStore<DoggoDocumentView> for SqlStorage {
             _ => schema_id.as_str(),
         };
 
-        let operation_inserted = query(
+        // Insert document view fields into the db
+        let document_view_inserted = query(
             "
             INSERT INTO
                 document_views (
@@ -194,9 +209,13 @@ impl DocumentStore<DoggoDocumentView> for SqlStorage {
         .map_err(|e| DocumentViewStorageError::Custom(e.to_string()))?
         .rows_affected()
             == 1;
-        Ok(field_relations_inserted && operation_inserted)
+        Ok(field_relations_inserted && document_view_inserted)
     }
 
+    /// Get a document view from the db by it'd id.
+    ///
+    /// Currently returns a map of document view fields as FieldName -> OperationValue.
+    /// This can be specified more shortly.
     async fn get_document_view_by_id(
         &self,
         id: &DocumentViewId,
@@ -366,40 +385,52 @@ mod tests {
         let storage_provider = SqlStorage { pool };
         let author = Author::new(TEST_AUTHOR).unwrap();
 
+        // Fake id for our first operation.
         let operation_id = OperationId::new(DEFAULT_HASH.parse().unwrap());
+
+        // The test CREATE operation which contains all fields.
         let operation = test_operation();
+
+        // The document view now is just this one operation.
         let document_view_id: DocumentViewId = operation_id.clone().into();
+
         let schema_id = SchemaId::from_str(TEST_SCHEMA_ID).unwrap();
 
+        // Init the field ids.
         let mut field_ids = FieldIds::new();
 
+        // Right now every field is derived from the CREATE operation, so they will all contain that id
         operation.fields().unwrap().keys().iter().for_each(|key| {
             field_ids.insert(key.clone(), operation_id.clone());
         });
 
+        // Construct a doggo operation for publishing.
         let doggo_operation = DoggoOperation::new(&operation, &operation_id, &author);
 
+        // Insert the CREATE op.
         storage_provider
             .insert_operation(&doggo_operation)
             .await
             .unwrap();
 
+        // Insert the document view, passing in the field_ids created above.
         storage_provider
             .insert_document_view(&document_view_id, &field_ids, &schema_id)
             .await
             .unwrap();
 
+        // Retrieve the document view.
         let result = storage_provider
             .get_document_view_by_id(&document_view_id)
             .await;
 
         println!("{:#?}", result);
 
+        // Construct an UPDATE operation which only updates one field.
         let mut fields = OperationFields::new();
         fields
             .add("username", OperationValue::Text("yahoooo".to_owned()))
             .unwrap();
-
         let update_operation = Operation::new_update(
             SchemaId::from_str(TEST_SCHEMA_ID).unwrap(),
             vec![operation_id],
@@ -407,25 +438,33 @@ mod tests {
         )
         .unwrap();
 
+        // Give it a dummy id.
         let update_operation_id = OperationId::new(
             "0020cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc"
                 .parse()
                 .unwrap(),
         );
+
+        // Update the field_ids to include the newly update operation_id for the "username" field.
         field_ids.insert("username".to_string(), update_operation_id.clone());
         let doggo_update_operation =
             DoggoOperation::new(&update_operation, &update_operation_id, &author);
 
+        // Insert the operation.
         storage_provider
             .insert_operation(&doggo_update_operation)
             .await
             .unwrap();
 
+        // Update the document view.
         storage_provider
             .insert_document_view(&update_operation_id.clone().into(), &field_ids, &schema_id)
             .await
             .unwrap();
 
+        // Query the new document view.
+        //
+        // It will combine the origin fields with the newly updated "username" field and return the completed fields.
         let result = storage_provider
             .get_document_view_by_id(&update_operation_id.into())
             .await;
