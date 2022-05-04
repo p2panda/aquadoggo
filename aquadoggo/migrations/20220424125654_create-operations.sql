358fd4156baba562e8e85339ed93048d8eaeda05
Sam Andreae <contact@samandreae.com>
2022-05-05 00:19:45 +0900

    Add foreign keys in operation tables

--- aquadoggo/migrations/20220424125654_create-operations.sql
+++ aquadoggo/migrations/20220424125654_create-operations.sql
@@ -8,6 +8,7 @@ CREATE TABLE IF NOT EXISTS operations_v1 (
     action              VARCHAR(16)       NOT NULL,
     schema_id           TEXT              NOT NULL,
     previous_operations TEXT              NULL,
+    -- FOREIGN KEY(entry_hash) REFERENCES entries(entry_hash),
     PRIMARY KEY (operation_id)
 );
 
@@ -15,14 +16,17 @@ CREATE TABLE IF NOT EXISTS operations_v1 (
 -- relation table. Can we remove it or are there reasons it's useful?
 CREATE TABLE IF NOT EXISTS previous_operations_v1 (
     parent_operation_id    VARCHAR(64)       NOT NULL,
-    child_operation_id     VARCHAR(64)       NOT NULL
+    child_operation_id     VARCHAR(64)       NOT NULL,
+    FOREIGN KEY(parent_operation_id) REFERENCES operations_v1(operation_id)
+    FOREIGN KEY(child_operation_id)  REFERENCES operations_v1(operation_id)
 );
 
 CREATE TABLE IF NOT EXISTS operation_fields_v1 (
     operation_id           VARCHAR(64)       NOT NULL,
     name                   VARCHAR(128)      NOT NULL,
     field_type             TEXT              NOT NULL,
-    value                  BLOB              NULL
+    value                  BLOB              NOT NULL,
+    FOREIGN KEY(operation_id) REFERENCES operations_v1(operation_id)
 );
 
 CREATE INDEX idx_operation_fields_v1 ON operation_fields_v1 (operation_id, name);
