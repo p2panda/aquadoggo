3985cd260527288f9935865f8e2ee1e225ef32c2
Sam Andreae <contact@samandreae.com>
2022-05-09 18:02:05 +0900

    Add placeholder operation store error

---
+++ aquadoggo/src/db/errors.rs
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+/// `OperationStore` errors.
+#[derive(thiserror::Error, Debug)]
+pub enum OperationStorageError {
+    /// Catch all error which implementers can use for passing their own errors up the chain.
+    #[error("Ahhhhh!!!!: {0}")]
+    Custom(String),
+}
