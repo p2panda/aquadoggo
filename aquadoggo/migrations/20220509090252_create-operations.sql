7e11cf79c68455006da2028ae596e28fce8222cc
Sam Andreae <contact@samandreae.com>
2022-05-09 18:03:58 +0900

    Create SQL migration for operations

---
+++ aquadoggo/migrations/20220509090252_create-operations.sql
@@ -0,0 +1,31 @@
+-- SPDX-License-Identifier: AGPL-3.0-or-later
+
+CREATE TABLE IF NOT EXISTS operations_v1 (
+    author              VARCHAR(64)       NOT NULL,
+    document_id         VARCHAR(64)       NOT NULL,
+    operation_id        VARCHAR(64)       NOT NULL UNIQUE,
+    entry_hash          VARCHAR(68)       NOT NULL UNIQUE,
+    action              VARCHAR(16)       NOT NULL,
+    schema_id           TEXT              NOT NULL,
+    previous_operations TEXT              NULL,
+    PRIMARY KEY (operation_id)
+);
+
+-- With the above "previous_operations" column in operations_v1 table we may no longer need this 
+-- relation table. Can we remove it or are there reasons it's useful?
+CREATE TABLE IF NOT EXISTS previous_operations_v1 (
+    parent_operation_id    VARCHAR(64)       NOT NULL,
+    child_operation_id     VARCHAR(64)       NOT NULL,
+    FOREIGN KEY(parent_operation_id) REFERENCES operations_v1(operation_id)
+    FOREIGN KEY(child_operation_id)  REFERENCES operations_v1(operation_id)
+);
+
+CREATE TABLE IF NOT EXISTS operation_fields_v1 (
+    operation_id           VARCHAR(64)       NOT NULL,
+    name                   VARCHAR(128)      NOT NULL,
+    field_type             TEXT              NOT NULL,
+    value                  BLOB              NULL,
+    FOREIGN KEY(operation_id) REFERENCES operations_v1(operation_id)
+);
+
+CREATE INDEX idx_operation_fields_v1 ON operation_fields_v1 (operation_id, name);
