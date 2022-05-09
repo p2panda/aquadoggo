1d0d8a45e18bcbe0ae38798d615b8c71f523a492
Sam Andreae <contact@samandreae.com>
2022-05-09 16:25:17 +0900

    Remove JSON RPC services

--- aquadoggo/src/rpc/mod.rs
+++ aquadoggo/src/rpc/mod.rs
@@ -1,12 +1,7 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
-mod api;
-mod methods;
 mod request;
 mod response;
-mod server;
 
-pub use api::{build_rpc_api_service, RpcApiService, RpcApiState};
 pub use request::{EntryArgsRequest, PublishEntryRequest};
 pub use response::{EntryArgsResponse, PublishEntryResponse};
-pub use server::{handle_get_http_request, handle_http_request};
