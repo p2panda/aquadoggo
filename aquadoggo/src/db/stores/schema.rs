2deba0e34421ee0408459028a6234a88faa0308b
Sam Andreae <contact@samandreae.com>
2022-05-19 13:50:21 +0900

    Sketch out SchemaStore API

--- aquadoggo/src/db/stores/schema.rs
+++ aquadoggo/src/db/stores/schema.rs
@@ -1,9 +1,19 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
 use async_trait::async_trait;
-use p2panda_rs::schema::Schema;
+use p2panda_rs::schema::{Schema, SchemaId};
+
+/// `SchemaStore` errors.
+#[derive(thiserror::Error, Debug)]
+pub enum SchemaStoreError {
+    /// Catch all error which implementers can use for passing their own errors up the chain.
+    #[error("Error occured in DocumentStore: {0}")]
+    Custom(String),
+}
 
 #[async_trait]
 pub trait SchemaStore {
-    // async fn insert_schema(&self, schema: &Schema) -> Result<(), SchemaStoreError>;
+    async fn insert_schema(&self, schema: &Schema) -> Result<(), SchemaStoreError>;
+
+    async fn get_schema_by_id(&self, id: &SchemaId) -> Result<Schema, SchemaStoreError>;
 }
