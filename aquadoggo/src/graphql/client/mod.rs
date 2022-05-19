4b5beca7b1d91d34d079d0157e863f79a58ed928
Vincent Ahrend <mail@vincentahrend.com>
2022-05-19 17:07:14 +0200

    Adopt async-graphql naming

--- aquadoggo/src/graphql/client/mod.rs
+++ aquadoggo/src/graphql/client/mod.rs
@@ -1,12 +1,12 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
-mod mutations;
-mod queries;
+mod mutation;
+mod query;
 mod request;
 mod response;
 pub(crate) mod u64_string;
 
-pub use mutations::Mutation;
-pub use queries::Query;
+pub use mutation::Mutation;
+pub use query::Query;
 pub use request::{EntryArgsRequest, PublishEntryRequest};
 pub use response::{EntryArgsResponse, PublishEntryResponse};
