e5ea4b2987b8d179be44aa7a96d45b2e76a83e1a
Sam Andreae <contact@samandreae.com>
2022-05-19 17:42:57 +0900

    Use TEXT types for string fields in all operation tables

--- aquadoggo/migrations/20220509090252_create-operations.sql
+++ aquadoggo/migrations/20220509090252_create-operations.sql
@@ -1,28 +1,28 @@
 -- SPDX-License-Identifier: AGPL-3.0-or-later
 
 CREATE TABLE IF NOT EXISTS operations_v1 (
-    author              VARCHAR(64)       NOT NULL,
-    document_id         VARCHAR(64)       NOT NULL,
-    operation_id        VARCHAR(64)       NOT NULL UNIQUE,
-    entry_hash          VARCHAR(68)       NOT NULL UNIQUE,
-    action              VARCHAR(16)       NOT NULL,
-    schema_id           TEXT              NOT NULL,
-    previous_operations TEXT              NULL,
+    author                  TEXT            NOT NULL,
+    document_id             TEXT            NOT NULL,
+    operation_id            TEXT            NOT NULL UNIQUE,
+    entry_hash              TEXT            NOT NULL UNIQUE,
+    action                  TEXT            NOT NULL,
+    schema_id               TEXT            NOT NULL,
+    previous_operations     TEXT            NULL,
     PRIMARY KEY (operation_id)
 );
 
 -- With the above "previous_operations" column in operations_v1 table we may no longer need this 
 -- relation table. Can we remove it or are there reasons it's useful?
 CREATE TABLE IF NOT EXISTS previous_operations_v1 (
-    parent_operation_id    VARCHAR(64)       NOT NULL,
-    child_operation_id     VARCHAR(64)       NOT NULL
+    parent_operation_id     TEXT             NOT NULL,
+    child_operation_id      TEXT             NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS operation_fields_v1 (
-    operation_id           VARCHAR(64)       NOT NULL,
-    name                   VARCHAR(128)      NOT NULL,
-    field_type             TEXT              NOT NULL,
-    value                  BLOB              NULL,
+    operation_id            TEXT             NOT NULL,
+    name                    TEXT             NOT NULL,
+    field_type              TEXT             NOT NULL,
+    value                   BLOB             NULL,
     FOREIGN KEY(operation_id) REFERENCES operations_v1(operation_id)
 );
 
