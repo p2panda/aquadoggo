dd2424a2d0956357bdd17eab7d92503abd8902ab
Sam Andreae <contact@samandreae.com>
2022-05-23 14:09:07 +0900

    Define get_all_schema() method

--- aquadoggo/src/db/stores/schema.rs
+++ aquadoggo/src/db/stores/schema.rs
@@ -1,19 +1,19 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
-use std::convert::TryInto;
+use std::convert::{TryFrom, TryInto};
 
 use async_trait::async_trait;
 use p2panda_rs::{
     document::{DocumentView, DocumentViewId},
     schema::{
         system::{SchemaFieldView, SchemaView, SystemSchemaError},
-        Schema, SchemaError,
+        Schema, SchemaError, SchemaId, SchemaIdError,
     },
 };
 
 use crate::db::{
-    models::document::DocumentViewFieldRow, provider::SqlStorage,
-    utils::parse_document_view_field_rows,
+    errors::DocumentStorageError, models::document::DocumentViewFieldRow, provider::SqlStorage,
+    traits::DocumentStore, utils::parse_document_view_field_rows,
 };
 
 /// `SchemaStore` errors.
@@ -27,14 +27,24 @@ pub enum SchemaStoreError {
     #[error(transparent)]
     SystemSchemaError(#[from] SystemSchemaError),
 
-    /// Error returned from converting p2panda-rs `DocumentView` into `SchemaView.
+    /// Error returned from p2panda-rs `Schema` methods.
     #[error(transparent)]
     SchemaError(#[from] SchemaError),
+
+    /// Error returned from p2panda-rs `Schema` methods.
+    #[error(transparent)]
+    SchemaIdError(#[from] SchemaIdError),
+
+    /// Error returned from `DocumentStore` methods.
+    #[error(transparent)]
+    DocumentStorageError(#[from] DocumentStorageError),
 }
 
 #[async_trait]
 pub trait SchemaStore {
     async fn get_schema_by_id(&self, id: &DocumentViewId) -> Result<Schema, SchemaStoreError>;
+
+    async fn get_all_schema(&self) -> Result<Vec<Schema>, SchemaStoreError>;
 }
 
 #[async_trait]
@@ -60,5 +70,40 @@ impl SchemaStore for SqlStorage {
         let schema = Schema::new(schema_view, schema_fields)?;
 
         Ok(schema)
+    }
+
+    async fn get_all_schema(&self) -> Result<Vec<Schema>, SchemaStoreError> {
+        let schema_views: Vec<SchemaView> = self
+            .get_documents_by_schema(&SchemaId::new("schema_definition_v1")?)
+            .await?
+            .into_iter()
+            .filter_map(|view| SchemaView::try_from(view).ok())
+            .collect();
+
+        let schema_field_views: Vec<SchemaFieldView> = self
+            .get_documents_by_schema(&SchemaId::new("schema_field_definition_v1")?)
+            .await?
+            .into_iter()
+            .filter_map(|view| SchemaFieldView::try_from(view).ok())
+            .collect();
+
+        let mut all_schema = vec![];
+
+        for schema_view in schema_views {
+            let schema_fields: Vec<SchemaFieldView> = schema_view
+                .fields()
+                .iter()
+                .filter_map(|field_id| {
+                    schema_field_views
+                        .iter()
+                        .find(|view| view.id() == &field_id)
+                })
+                .map(|field| field.to_owned())
+                .collect();
+
+            all_schema.push(Schema::new(schema_view, schema_fields)?);
+        }
+
+        Ok(all_schema)
     }
 }
