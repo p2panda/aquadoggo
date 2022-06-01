0884acdaa3a2383be3d27a3a418941552ccc5c6b
Sam Andreae <contact@samandreae.com>
2022-06-01 09:59:22 +0100

    Implement `DocumentStore` (#133)
    * Create document sql migrations
    
    * Refactor traits and create DocumentStore
    
    * WIP: DocumentStore
    
    * WIP: More work on getting document views from db
    
    * WIP: ongoing changes
    
    * Use p2panda-rs DocumenFieldView branch
    
    * Integrate new DocumentView API
    
    * Fix messed up operation query string
    
    * Remove comment
    
    * Comment out unused structs
    
    * Re-introduce StorageDocumentView trait and struct
    
    * Housekeeping
    
    * Bump p2panda-rs
    
    * Refactor tests a bit
    
    * Tidy imports a bit
    
    * Update comments
    
    * Refactor insertion error handling
    
    * Refactor tests
    
    * Test for failing insert
    
    * Bump p2panda-rs branch
    
    * Fix method name mistakes
    
    * Some test re-jigging
    
    * Doc strings and test for utils.rs
    
    * Test for parsing document view fields
    
    * Some test comments
    
    * Fixes after rebase
    
    * Update p2panda-rs api
    
    * Add is_deleted column to documents table
    
    * Bump Cargo.lock
    
    * Bump p2panda-rs branch
    
    * Insert Documents into the db
    
    * Method for getting documents from storage
    
    * Improve error handling
    
    * Handle deleted documents
    
    * Implement multiple authors in test_db
    
    * Update entry store trait method naming
    
    * Improvements in test_db
    
    * Update insert entry method signature
    
    * Some tidy up
    
    * return an option from get_document_view_by_id()
    
    * Doc strings for all
    
    * Update operation store doc string
    
    * Remove storage document and view traits and specialist type requirements
    
    * Fixes after rebase
    
    * Move raw_operation() out of trait impl block
    
    * Update CHANGELOG
    
    * Remove unused error
    
    * Tidy imports
    
    * Use TEXT type for all strings in document db tables
    
    * Fix imports after merge
    
    * Improve doc strings
    
    * Fix get_documents_by_schema test
    
    * Add failing test for multiple relation fields
    
    * Handle parsing document views with multiple relation list fields
    
    * Add doc strings for test_db method
    
    * Correct doc string
    
    * Correct CHANGELOG
    
    * Fix import order
    
    * Add missing license header
    
    * Fix SQL string indentation
    
    * Little more indentation fix
    
    * Add TODO flag
    
    * Make clippy happy
    
    Co-authored-by: Andreas Dzialocha <x12@adz.garden>

---
+++ aquadoggo/src/db/errors.rs
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+use p2panda_rs::document::{DocumentId, DocumentViewId};
+
+/// `DocumentStore` errors.
+#[derive(thiserror::Error, Debug)]
+pub enum DocumentStorageError {
+    /// Catch all error which implementers can use for passing their own errors up the chain.
+    #[allow(dead_code)]
+    #[error("Error occured in DocumentStore: {0}")]
+    Custom(String),
+
+    /// A fatal error occured when performing a storage query.
+    #[error("A fatal error occured in DocumentStore: {0}")]
+    FatalStorageError(String),
+
+    /// Error which originates in `insert_document_view()` when the insertion fails.
+    #[error("Error occured when inserting a document view with id {0:?} into storage")]
+    DocumentViewInsertionError(DocumentViewId),
+
+    /// Error which originates in `insert_document()` when the insertion fails.
+    #[error("Error occured when inserting a document with id {0:?} into storage")]
+    DocumentInsertionError(DocumentId),
+}
--- aquadoggo/src/db/stores/test_utils.rs
+++ aquadoggo/src/db/stores/test_utils.rs
@@ -12,15 +12,19 @@ use p2panda_rs::operation::{
     PinnedRelationList, Relation, RelationList,
 };
 use p2panda_rs::schema::SchemaId;
-use p2panda_rs::storage_provider::traits::OperationStore;
-use p2panda_rs::storage_provider::traits::StorageProvider;
+use p2panda_rs::storage_provider::traits::{
+    AsStorageEntry, AsStorageLog, LogStore, StorageProvider,
+};
+use p2panda_rs::storage_provider::traits::{EntryStore, OperationStore};
 use p2panda_rs::test_utils::constants::{DEFAULT_PRIVATE_KEY, TEST_SCHEMA_ID};
 
 use crate::db::provider::SqlStorage;
-use crate::graphql::client::{EntryArgsRequest, PublishEntryRequest};
+use crate::graphql::client::EntryArgsRequest;
 use crate::test_helpers::initialize_db;
 
-use super::operation::OperationStorage;
+use crate::db::stores::OperationStorage;
+use crate::db::stores::StorageEntry;
+use crate::db::stores::StorageLog;
 
 pub fn test_create_operation() -> Operation {
     let mut fields = OperationFields::new();
@@ -82,6 +86,19 @@ pub fn test_create_operation() -> Operation {
             ])),
         )
         .unwrap();
+    fields
+        .add(
+            "another_relation_field",
+            OperationValue::PinnedRelationList(PinnedRelationList::new(vec![
+                Hash::new("0020abababababababababababababababababababababababababababababababab")
+                    .unwrap()
+                    .into(),
+                Hash::new("0020cdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcd")
+                    .unwrap()
+                    .into(),
+            ])),
+        )
+        .unwrap();
     Operation::new_create(SchemaId::from_str(TEST_SCHEMA_ID).unwrap(), fields).unwrap()
 }
 
@@ -107,80 +124,110 @@ pub fn test_delete_operation(previous_operations: Vec<OperationId>) -> Operation
     .unwrap()
 }
 
-pub async fn test_db(no_of_entries: usize, with_delete: bool) -> SqlStorage {
+pub fn test_key_pairs(no_of_authors: usize) -> Vec<KeyPair> {
+    let mut key_pairs = vec![KeyPair::from_private_key_str(DEFAULT_PRIVATE_KEY).unwrap()];
+
+    for _index in 1..no_of_authors {
+        key_pairs.push(KeyPair::new())
+    }
+
+    key_pairs
+}
+
+/// Construct and return a storage provider instance backed by a pre-polpulated database. Passed parameters
+/// define what the db should contain. The first entry in each log contains a valid CREATE operation
+/// following entries contain duplicate UPDATE operations. If the with_delete flag is set to true
+/// the last entry in all logs contain be a DELETE operation.
+///
+/// Returns a storage provider instance, a vector of key pairs for all authors in the db, and a vector
+/// of the ids for all documents.
+pub async fn test_db(
+    // Number of entries per log/document
+    no_of_entries: usize,
+    // Number of authors, each with a log populated as defined above
+    no_of_authors: usize,
+    // A boolean flag for wether all logs should contain a delete operation
+    with_delete: bool,
+) -> (SqlStorage, Vec<KeyPair>, Vec<DocumentId>) {
     let pool = initialize_db().await;
     let storage_provider = SqlStorage { pool };
 
     // If we don't want any entries in the db return now
     if no_of_entries == 0 {
-        return storage_provider;
+        return (storage_provider, Vec::default(), Vec::default());
     }
 
-    let key_pair = KeyPair::from_private_key_str(DEFAULT_PRIVATE_KEY).unwrap();
-    let author = Author::try_from(key_pair.public_key().to_owned()).unwrap();
+    let mut documents: Vec<DocumentId> = Vec::new();
+    let schema = SchemaId::from_str(TEST_SCHEMA_ID).unwrap();
+    let key_pairs = test_key_pairs(no_of_authors);
 
-    let mut document: Option<DocumentId> = None;
+    for key_pair in &key_pairs {
+        let mut document: Option<DocumentId> = None;
+        let author = Author::try_from(key_pair.public_key().to_owned()).unwrap();
+        for index in 0..no_of_entries {
+            let next_entry_args = storage_provider
+                .get_entry_args(&EntryArgsRequest {
+                    author: author.clone(),
+                    document: document.as_ref().cloned(),
+                })
+                .await
+                .unwrap();
 
-    // Publish more update entries
-    for index in 0..no_of_entries {
-        let next_entry_args = storage_provider
-            .get_entry_args(&EntryArgsRequest {
-                author: author.clone(),
-                document: document.as_ref().cloned(),
-            })
-            .await
-            .unwrap();
+            let next_operation = if index == 0 {
+                test_create_operation()
+            } else if index == (no_of_entries - 1) && with_delete {
+                test_delete_operation(vec![next_entry_args.backlink.clone().unwrap().into()])
+            } else {
+                test_update_operation(
+                    vec![next_entry_args.backlink.clone().unwrap().into()],
+                    "yoyo",
+                )
+            };
 
-        let next_operation = if index == 0 {
-            test_create_operation()
-        } else if index == no_of_entries && with_delete {
-            test_delete_operation(vec![next_entry_args.backlink.clone().unwrap().into()])
-        } else {
-            test_update_operation(
-                vec![next_entry_args.backlink.clone().unwrap().into()],
-                "yoyo",
+            let next_entry = Entry::new(
+                &next_entry_args.log_id,
+                Some(&next_operation),
+                next_entry_args.backlink.as_ref(),
+                next_entry_args.backlink.as_ref(),
+                &next_entry_args.seq_num,
             )
-        };
+            .unwrap();
 
-        let next_entry = Entry::new(
-            &next_entry_args.log_id,
-            Some(&next_operation),
-            next_entry_args.skiplink.as_ref(),
-            next_entry_args.backlink.as_ref(),
-            &next_entry_args.seq_num,
-        )
-        .unwrap();
+            let entry_encoded = sign_and_encode(&next_entry, key_pair).unwrap();
+            let operation_encoded = OperationEncoded::try_from(&next_operation).unwrap();
 
-        let entry_encoded = sign_and_encode(&next_entry, &key_pair).unwrap();
-        let operation_encoded = OperationEncoded::try_from(&next_operation).unwrap();
+            if index == 0 {
+                document = Some(entry_encoded.hash().into());
+                documents.push(entry_encoded.hash().into());
+            }
 
-        if index == 0 {
-            document = Some(entry_encoded.hash().into());
+            let storage_entry = StorageEntry::new(&entry_encoded, &operation_encoded).unwrap();
+
+            storage_provider.insert_entry(storage_entry).await.unwrap();
+
+            let storage_log = StorageLog::new(
+                &author,
+                &schema,
+                &document.clone().unwrap(),
+                &next_entry_args.log_id,
+            );
+
+            if next_entry_args.seq_num.is_first() {
+                storage_provider.insert_log(storage_log).await.unwrap();
+            }
+
+            let storage_operation = OperationStorage::new(
+                &author,
+                &next_operation,
+                &entry_encoded.hash().into(),
+                &document.as_ref().cloned().unwrap(),
+            );
+
+            storage_provider
+                .insert_operation(&storage_operation)
+                .await
+                .unwrap();
         }
-
-        let operation_id = entry_encoded.hash().into();
-
-        // Publish the new entry
-        storage_provider
-            .publish_entry(&PublishEntryRequest {
-                entry_encoded: entry_encoded.clone(),
-                operation_encoded,
-            })
-            .await
-            .unwrap();
-
-        let storage_operation = OperationStorage::new(
-            &author,
-            &next_operation,
-            &operation_id,
-            &document.as_ref().cloned().unwrap(),
-        );
-
-        // Publish the new entry
-        storage_provider
-            .insert_operation(&storage_operation)
-            .await
-            .unwrap();
     }
-    storage_provider
+    (storage_provider, key_pairs, documents)
 }
