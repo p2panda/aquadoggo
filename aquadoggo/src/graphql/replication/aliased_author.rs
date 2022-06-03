b1331bd7812e85adb9a40c12c1a491e60882e42c
Piet Geursen <pietgeursen@gmail.com>
2022-06-03 14:34:08 +1200

    Add docstring to the `AliasedAuthor` type.

--- aquadoggo/src/graphql/replication/aliased_author.rs
+++ aquadoggo/src/graphql/replication/aliased_author.rs
@@ -4,6 +4,14 @@ use async_graphql::*;
 
 use super::public_key::PublicKey;
 
+/// AliasedAuthor is one of either the public_key or an alias
+///
+/// The intention of this is to reduce bandwidth when making requests by using a short "alias"
+/// rather than the full author public_key
+///
+/// To get an alias of an author, use the `author_aliases` method which will return this type.
+///
+/// When using as an input to a query, exactly one of public_key or alias must be set otherwise it is an error.
 #[derive(Debug, InputObject, SimpleObject)]
 pub struct AliasedAuthor {
     /// The author's public key
