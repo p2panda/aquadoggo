4236230889d5b745142951180ccf4449b604aeb4
Andreas Dzialocha <x12@adz.garden>
2022-06-23 13:45:24 +0200

    Bring back failing UNIQUE constraint

--- aquadoggo/migrations/20220617115933_create-tasks.sql
+++ aquadoggo/migrations/20220617115933_create-tasks.sql
@@ -9,12 +9,11 @@ CREATE TABLE IF NOT EXISTS tasks (
 -- Create a unique index using `COALESCE`. A regular `UNIQUE` clause will
 -- consider two rows that have at least one `null` value to always be distinct
 -- but we want to check for equality including `null` values.
--- @TODO: This fails using PostgreSQL
--- CREATE UNIQUE INDEX ux_tasks ON tasks (
-    --    name,
-    --    COALESCE(document_id, 0),
-    --    COALESCE(document_view_id, 0)
-    --);
+CREATE UNIQUE INDEX ux_tasks ON tasks (
+    name,
+    COALESCE(document_id, 0),
+    COALESCE(document_view_id, 0)
+);
 
 -- Create an index because primary keys can not contain `null` columns.
 CREATE INDEX idx_tasks ON tasks (name, document_id, document_view_id);
