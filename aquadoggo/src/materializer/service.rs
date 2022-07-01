a0af183deba424810872e85abd75a50df0440673
Sam Andreae <contact@samandreae.com>
2022-07-01 11:41:22 +0100

    p2panda-rs v0.4.0

--- aquadoggo/src/materializer/service.rs
+++ aquadoggo/src/materializer/service.rs
@@ -141,8 +141,7 @@ mod tests {
     use p2panda_rs::storage_provider::traits::OperationStore;
     use p2panda_rs::test_utils::constants::TEST_SCHEMA_ID;
     use p2panda_rs::test_utils::fixtures::{
-        key_pair, operation, operation_fields, random_document_id,
-        random_operation_id,
+        key_pair, operation, operation_fields, random_document_id, random_operation_id,
     };
     use rstest::rstest;
     use tokio::sync::broadcast;
