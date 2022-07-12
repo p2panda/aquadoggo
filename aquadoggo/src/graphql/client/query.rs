59c545b389b40810880a5b44c93a3bc78aa9ff61
Sam Andreae <contact@samandreae.com>
2022-07-12 17:29:37 +0200

    Refactor next_entry_args

--- aquadoggo/src/graphql/client/query.rs
+++ aquadoggo/src/graphql/client/query.rs
@@ -1,14 +1,16 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
-use async_graphql::{Context, Error, Object, Result};
-use p2panda_rs::document::DocumentId;
-use p2panda_rs::storage_provider::traits::StorageProvider;
+use async_graphql::{Context, Object, Result};
+use p2panda_rs::document::DocumentViewId;
+use p2panda_rs::entry::SeqNum;
+use p2panda_rs::identity::Author;
+use p2panda_rs::storage_provider::traits::{AsStorageEntry, EntryStore, LogStore};
 use p2panda_rs::Validate;
 
 use crate::db::provider::SqlStorage;
-use crate::db::request::EntryArgsRequest;
 use crate::graphql::client::response::NextEntryArguments;
 use crate::graphql::scalars;
+use crate::validation::get_validate_document_id_for_view_id;
 
 /// GraphQL queries for the Client API.
 #[derive(Default, Debug, Copy, Clone)]
@@ -31,17 +33,70 @@ impl ClientRoot {
             desc = "Document the entry's UPDATE or DELETE operation is referring to, \
             can be left empty when it is a CREATE operation"
         )]
-        document_id: Option<scalars::DocumentId>,
+        document_view_id: Option<scalars::DocumentViewId>,
     ) -> Result<NextEntryArguments> {
-        let args = EntryArgsRequest {
-            public_key: public_key.into(),
-            document_id: document_id.map(DocumentId::from),
-        };
-        args.validate()?;
-
-        // Load and return next entry arguments
+        // Access the store from context.
         let store = ctx.data::<SqlStorage>()?;
-        store.get_entry_args(&args).await.map_err(Error::from)
+
+        // Validate the public key.
+        let public_key: Author = public_key.into();
+        public_key.validate()?;
+
+        let document_view_id = document_view_id.map(DocumentViewId::from);
+
+        // If no document_view_id is passed then this is a request for publishing a CREATE operation
+        // and we return the args for the next free log by this author.
+        if document_view_id.is_none() {
+            let log_id = store.next_log_id(&public_key).await?;
+            return Ok(NextEntryArguments {
+                backlink: None,
+                skiplink: None,
+                seq_num: SeqNum::default().into(),
+                log_id: log_id.into(),
+            });
+        }
+
+        // Access and validate the document view id.
+        //
+        // We can unwrap here as we know document_view_id is some.
+        let document_view_id: DocumentViewId = document_view_id.unwrap();
+        document_view_id.validate()?;
+
+        // Get the document_id for this document_view_id. This performs several validation steps (check
+        // method doc string).
+        let document_id = get_validate_document_id_for_view_id(&store, &document_view_id).await?;
+
+        // Retrieve the log_id for the found document_id and author.
+        //
+        // (lolz, this method is just called `get()`)
+        let log_id = match store.get(&public_key, &document_id).await? {
+            // This public key already wrote to this document, so we return the found log_id
+            Some(log_id) => log_id,
+            // This public_key never wrote to this document before so we return a new log_id
+            None => store.next_log_id(&public_key).await?,
+        };
+
+        // Get the backlink by getting the latest entry in this log.
+        let backlink = store
+            .get_latest_entry(&public_key, &log_id)
+            .await?
+            .expect("All logs must contain at least one entry");
+
+        // Determine skiplink ("lipmaa"-link) entry in this log.
+        let skiplink_hash = store.determine_next_skiplink(&backlink).await?;
+
+        // Determine the next sequence number.
+        let seq_num = backlink
+            .seq_num()
+            .next()
+            .expect("Max sequence number reached \\*o*/");
+
+        Ok(NextEntryArguments {
+            backlink: Some(backlink.hash().to_owned().into()),
+            skiplink: skiplink_hash.map(|hash| hash.into()),
+            seq_num: seq_num.into(),
+            log_id: log_id.into(),
+        })
     }
 }
 
