2845c8eddbdd67fecdb857b3276d7e4c371a796b
Vincent Ahrend <mail@vincentahrend.com>
2022-07-14 23:44:08 +0200

    Rename module

--- aquadoggo/src/graphql/mod.rs
+++ aquadoggo/src/graphql/mod.rs
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
 pub mod client;
+mod graphql_schema;
 pub mod pagination;
 pub mod replication;
 pub mod scalars;
-mod schema;
 
-pub use schema::{build_root_schema, GraphQLSchemaManager, QueryRoot, RootSchema};
+pub use graphql_schema::{build_root_schema, GraphQLSchemaManager, QueryRoot, RootSchema};
