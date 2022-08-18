b22baf25cbca606172cf525db20108bc11a1489c
Sam Andreae <contact@samandreae.com>
2022-08-18 20:25:25 +0100

    Small refactor in e2e test

--- aquadoggo/src/e2e_tests.rs
+++ aquadoggo/src/e2e_tests.rs
@@ -23,6 +23,115 @@ use serde_json::json;
 
 use crate::{Configuration, Node};
 
+#[tokio::test]
+async fn e2e() {
+    // Node configuration.
+    //
+    // We mostly go for default configuration options here. The only thing we want to do change
+    // is the database config. We want an in memory sqlite db for this test, the default id to use
+    // a postgres db.
+    let config = Configuration {
+        database_url: Some("sqlite::memory:".to_string()),
+        ..Default::default()
+    };
+
+    // Start the node.
+    //
+    // There's quite a lot happening under the hood here. We initialize the db, start a http server with
+    // GraphQL endpoints, a service for materializing documents from entries and operation which arrive
+    // at the node, and finally a replication service for communicating and replicating with other nodes.
+    //
+    // Nodes are the workhorses of the p2panda network, we thank you for all your efforts 🙏🏻.
+
+    let _my_hero = Node::start(config).await; // 🐬🐕
+
+    // Create some authors.
+    //
+    // This is not a very "real world" step as you would usually have stored your key pair somewhere
+    // safe and be loading it into an app. Probably not something you want to copy-paste into your hot
+    // new app ;-p
+
+    let panda = KeyPair::from_private_key_str(
+        "ddcafe34db2625af34c8ba3cf35d46e23283d908c9848c8b43d1f5d0fde779ea",
+    )
+    .unwrap();
+
+    // Create a GraphQL client.
+    //
+    // This is the client which will be communicating with the already running node. For this example we
+    // are running a node and client in the same application, in a real world setting they might be on the
+    // the same device, or equally they could be speaking across a network. This seperation is important
+    // for p2panda, as it's what enables the possibility to build very lightweight clients communicating
+    // with remote nodes who persist, replicate, materialize and serve data on the network.
+
+    let client = reqwest::Client::builder()
+        .redirect(reqwest::redirect::Policy::none())
+        .build()
+        .unwrap();
+
+    // Creating schema.
+    //
+    // All data in the p2panda network adheres to pre-defined schema. "That's great!" I hear you say,
+    // "but who defines the data defining schema?"🐣.... Good question. For that we have two hard-coded
+    // p2panda "system schema" (more will appear in the future, but this is enough for now). These are
+    // `schema_field_definition` and `schema_definition`. With these two anyone can publish their own
+    // schema to a node.
+    //
+    // Creating schema isn't something you do everytime you run your app. Like creating key pairs
+    // it's an initialization step which you (or someone else) does before you get going properly.
+    // You could even re-use schema's someone else already published.
+    //
+    // For that reason the details of this process are hidden here in this handy method. You know
+    // where to go if you want to know more.
+
+    let cafe_schema_id = create_cafe_schema(&client).await;
+
+    // Create a new document.
+    //
+    // Data in the p2panda network is encapsulated by something called a `document`. Documents can
+    // be CREATED, UPDATED and DELETED. We do this by publishing data mutations called `operations`
+    // which perform these actions.
+    //
+    // Once again, there are a few steps happening in the background here, look into the respective
+    // methods if you want to dig deeper.
+    //
+    // Here panda wants to create a new cafe called "Panda Cafe". In order to do this we construct
+    // and publish a CREATE operation with the full fields defined by the `cafe_schema`. Doing this
+    // will create a new document which is materialized on the node. It will only contain one
+    // operation right now.
+
+    let panda_operation_1 = OperationBuilder::new(&cafe_schema_id)
+        .action(OperationAction::Create)
+        .fields(&[("name", OperationValue::String("Panda Cafe".to_string()))])
+        .build()
+        .unwrap();
+
+    // Update a document.
+    //
+    // Panda want's more impact for their new cafe name, they want to add some drama!
+
+    // @TOOD: Keep going with the story.
+
+    let panda_cafe_view_id = publish(&client, &panda, &panda_operation_1).await;
+
+    let panda_operation_2 = OperationBuilder::new(&cafe_schema_id)
+        .action(OperationAction::Update)
+        .fields(&[("name", OperationValue::String("Panda Cafe!".to_string()))])
+        .previous_operations(&panda_cafe_view_id)
+        .build()
+        .unwrap();
+
+    let _panda_cafe_view_id = publish(&client, &panda, &panda_operation_2).await;
+
+    //     query(&client, &panda_cafe_view_id, &cafe_schema_id).await;
+    //     query_view_id(&client, &panda_cafe_view_id, &cafe_schema_id).await;
+    //
+    //     let penguin = KeyPair::from_private_key_str(
+    //         "1c86b2524b48f0ba86103cddc6bdfd87774ab77ab4c0ea989ed0eeab3d28827a",
+    //     )
+    //     .unwrap();
+}
+
 async fn parse_next_args_response(
     response: Response,
     field: &str,
@@ -246,113 +355,4 @@ async fn create_cafe_schema(client: &Client) -> SchemaId {
     let schema_definition_id = publish(&client, &shirokuma, &create_schema_op).await;
     let cafe_schema_id = SchemaId::Application("cafe".to_string(), schema_definition_id);
     cafe_schema_id
-}
-
-#[tokio::test]
-async fn e2e_publish_test() {
-    // Node configuration.
-    //
-    // We mostly go for default configuration options here. The only thing we want to do change
-    // is the database config. We want an in memory sqlite db for this test, the default id to use
-    // a postgres db.
-    let config = Configuration {
-        database_url: Some("sqlite::memory:".to_string()),
-        ..Default::default()
-    };
-
-    // Start the node.
-    //
-    // There's quite a lot happening under the hood here. We initialize the db, start a http server with
-    // GraphQL endpoints, a service for materializing documents from entries and operation which arrive
-    // at the node, and finally a replication service for communicating and replicating with other nodes.
-    //
-    // Nodes are the workhorses of the p2panda network, we thank you for all your efforts 🙏🏻.
-
-    let _my_hero = Node::start(config).await; // 🐬🐕
-
-    // Create some authors.
-    //
-    // This is not a very "real world" step as you would usually have stored your key pair somewhere
-    // safe and be loading it into an app. Probably not something you want to copy-paste into your hot
-    // new app ;-p
-
-    let panda = KeyPair::from_private_key_str(
-        "ddcafe34db2625af34c8ba3cf35d46e23283d908c9848c8b43d1f5d0fde779ea",
-    )
-    .unwrap();
-
-    // Create a GraphQL client.
-    //
-    // This is the client which will be communicating with the already running node. For this example we
-    // are running a node and client in the same application, in a real world setting they might be on the
-    // the same device, or equally they could be speaking across a network. This seperation is important
-    // for p2panda, as it's what enables the possibility to build very lightweight clients communicating
-    // with remote nodes who persist, replicate, materialize and serve data on the network.
-
-    let client = reqwest::Client::builder()
-        .redirect(reqwest::redirect::Policy::none())
-        .build()
-        .unwrap();
-
-    // Creating schema.
-    //
-    // All data in the p2panda network adheres to pre-defined schema. "That's great!" I hear you say,
-    // "but who defines the data defining schema?"🐣.... Good question. For that we have two hard-coded
-    // p2panda "system schema" (more will appear in the future, but this is enough for now). These are
-    // `schema_field_definition` and `schema_definition`. With these two anyone can publish their own
-    // schema to a node.
-    //
-    // Creating schema isn't something you do everytime you run your app. Like creating key pairs
-    // it's an initialization step which you (or someone else) does before you get going properly.
-    // You could even re-use schema's someone else already published.
-    //
-    // For that reason the details of this process are hidden here in this handy method. You know
-    // where to go if you want to know more.
-
-    let cafe_schema_id = create_cafe_schema(&client).await;
-
-    // Create a new document.
-    //
-    // Data in the p2panda network is encapsulated by something called a `document`. Documents can
-    // be CREATED, UPDATED and DELETED. We do this by publishing data mutations called `operations`
-    // which perform these actions.
-    //
-    // Once again, there are a few steps happening in the background here, look into the respective
-    // methods if you want to dig deeper.
-    //
-    // Here panda wants to create a new cafe called "Panda Cafe". In order to do this we construct
-    // and publish a CREATE operation with the full fields defined by the `cafe_schema`. Doing this
-    // will create a new document which is materialized on the node. It will only contain one
-    // operation right now.
-
-    let panda_operation_1 = OperationBuilder::new(&cafe_schema_id)
-        .action(OperationAction::Create)
-        .fields(&[("name", OperationValue::String("Panda Cafe".to_string()))])
-        .build()
-        .unwrap();
-
-    // Update a document.
-    //
-    // Panda want's more impact for their new cafe name, they want to add some drama!
-
-    // @TOOD: Keep going with the story.
-
-    let panda_cafe_view_id = publish(&client, &panda, &panda_operation_1).await;
-
-    let panda_operation_2 = OperationBuilder::new(&cafe_schema_id)
-        .action(OperationAction::Update)
-        .fields(&[("name", OperationValue::String("Panda Cafe!".to_string()))])
-        .previous_operations(&panda_cafe_view_id)
-        .build()
-        .unwrap();
-
-    let _panda_cafe_view_id = publish(&client, &panda, &panda_operation_2).await;
-
-    query(&client, &panda_cafe_view_id, &cafe_schema_id).await;
-    query_view_id(&client, &panda_cafe_view_id, &cafe_schema_id).await;
-
-    let penguin = KeyPair::from_private_key_str(
-        "1c86b2524b48f0ba86103cddc6bdfd87774ab77ab4c0ea989ed0eeab3d28827a",
-    )
-    .unwrap();
 }
