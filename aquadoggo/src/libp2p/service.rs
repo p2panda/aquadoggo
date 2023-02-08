95010975ed96b4dd89ca4f1549e8d011fdfd5fdd
Sam Andreae <contact@samandreae.com>
2023-02-08 18:55:36 +0000

    Use swarm

--- aquadoggo/src/libp2p/service.rs
+++ aquadoggo/src/libp2p/service.rs
@@ -4,8 +4,10 @@ use anyhow::Result;
 use futures::StreamExt;
 use libp2p::core::muxing::StreamMuxerBox;
 use libp2p::core::transport::Boxed;
-use libp2p::{identity, PeerId, Transport};
-use libp2p::{quic, Multiaddr};
+use libp2p::ping::Event;
+use libp2p::swarm::{keep_alive, NetworkBehaviour, SwarmEvent};
+use libp2p::{identity, PeerId, Swarm, Transport};
+use libp2p::{ping, quic, Multiaddr};
 use log::{info, warn};
 use tokio::task::{self, JoinHandle};
 
@@ -14,17 +16,18 @@ use crate::context::Context;
 use crate::manager::{ServiceReadySender, Shutdown};
 
 /// Testing out using quic transport from libp2p.
-/// 
+///
 /// This service can run as either a listener or a dialer.
-/// 
+///
 /// First run as a listener like so:
-/// 
-/// `RUST_LOG=debug cargo run`
-/// 
-/// Then run as a dialer which attempts to dial the above listener:
-/// 
-/// `RUST_LOG=debug cargo run -- --http-port 2021 --remote-node-addresses "/ip4/127.0.0.1/udp/12345/quic-v1"`
-/// 
+///
+/// `RUST_LOG=info cargo run`
+///
+/// Then run as a dialer which attempts to dial the above listener, taking the correct multiaddr
+/// from the logging in the previous peer:
+///
+/// `RUST_LOG=info cargo run -- --http-port 2021 --remote-node-addresses "/ip4/127.0.0.1/udp/<PORT>/quic-v1"`
+///
 /// Actually, nothing happens right now.... not sure why yet ;-p
 pub async fn libp2p_service(
     context: Context,
@@ -37,27 +40,91 @@ pub async fn libp2p_service(
 
     // Create a random PeerId
     let keypair = identity::Keypair::generate_ed25519();
-    info!("Peer id: {:?}", PeerId::from(keypair.public()));
+    let peer_id = PeerId::from(keypair.public());
+    info!("Peer id: {peer_id:?}");
 
-    // Init quic transport
-    let quic_transport = create_quic_transport(keypair);
+    // Create a quic transport.
+    let quic_config = quic::Config::new(&keypair);
+    let quic_transport = quic::tokio::Transport::new(quic_config)
+        // Not sure why we need to do this conversion to a StreamMuxerBox here, but I found that
+        // it's necessary. Maybe it's because quic handles multiplexing for us already.
+        .map(|(p, c), _| (p, StreamMuxerBox::new(c)))
+        .boxed();
 
-    // If a remote peer multiaddress is given using the `--remote-node-addresses` flag then dial
-    // it otherwise start listening for connections.  
-    let handle = match context.config.replication.remote_peers.get(0) {
-        Some(peer) => {
-            let addr: Multiaddr = peer.parse().expect("address should be valid");
-            dial(&addr, quic_transport)
+    // Init swarm with our behaviour (`keep_alive` and `ping`) defined below
+    let mut swarm = Swarm::with_tokio_executor(quic_transport, Behaviour::default(), peer_id);
+
+    // Tell the swarm to listen on 127.0.0.1 and a random, OS-assigned
+    // port.
+    swarm.listen_on("/ip4/127.0.0.1/udp/0/quic-v1".parse()?)?;
+
+    // Dial the peer identified by the multi-address given in the `--remote-node-addresses` if given
+    if let Some(addr) = context.config.replication.remote_peers.get(0) {
+        let remote: Multiaddr = addr.parse()?;
+        swarm.dial(remote)?;
+        println!("Dialed {addr}")
+    }
+
+    // Spawn a task logging swarm events
+    let handle = tokio::spawn(async move {
+        loop {
+            match swarm.select_next_some().await {
+                SwarmEvent::NewListenAddr {
+                    address,
+                    listener_id,
+                } => {
+                    info!("Listening on {address:?} {listener_id:?}");
+                }
+                SwarmEvent::ConnectionEstablished {
+                    peer_id,
+                    endpoint,
+                    num_established,
+                    ..
+                } => info!("ConnectionEstablished: {peer_id:?} {endpoint:?} {num_established}"),
+                SwarmEvent::ConnectionClosed {
+                    peer_id,
+                    endpoint,
+                    num_established,
+                    cause,
+                } => {
+                    info!("ConnectionClosed: {peer_id:?} {endpoint:?} {num_established} {cause:?}")
+                }
+                SwarmEvent::IncomingConnection {
+                    local_addr,
+                    send_back_addr,
+                } => info!("IncomingConnection: {local_addr:?} {send_back_addr:?}"),
+                SwarmEvent::IncomingConnectionError {
+                    local_addr,
+                    send_back_addr,
+                    error,
+                } => info!("IncomingConnectionError: {local_addr:?} {send_back_addr:?} {error:?}"),
+                SwarmEvent::OutgoingConnectionError { peer_id, error } => {
+                    info!("OutgoingConnectionError: {peer_id:?} {error:?}")
+                }
+                SwarmEvent::BannedPeer { peer_id, endpoint } => info!("BannedPeer: {peer_id:?}"),
+                SwarmEvent::ExpiredListenAddr {
+                    listener_id,
+                    address,
+                } => info!("ExpiredListenAddr: {listener_id:?} {address:?}"),
+                SwarmEvent::ListenerClosed {
+                    listener_id,
+                    addresses,
+                    reason,
+                } => info!("ListenerClosed: {listener_id:?} {addresses:?} {reason:?}"),
+                SwarmEvent::ListenerError { listener_id, error } => {
+                    info!("ListenerError: {listener_id:?} {error:?}")
+                }
+                SwarmEvent::Dialing(peer_id) => info!("Dialing: {peer_id:?}"),
+                SwarmEvent::Behaviour(BehaviourEvent::KeepAlive(_)) => info!("Keep alive"),
+                SwarmEvent::Behaviour(BehaviourEvent::Ping(Event { peer, result })) => {
+                    info!("Ping from: {peer:?}")
+                }
+            };
         }
-        None => {
-            let addr: Multiaddr = "/ip4/127.0.0.1/udp/12345/quic-v1"
-                .parse()
-                .expect("address should be valid");
-            listen(&addr, quic_transport)
-        }
-    };
+    });
 
     info!("libp2p service is ready");
+
     if tx_ready.send(()).is_err() {
         warn!("No subscriber informed about libp2p service being ready");
     };
@@ -72,64 +139,12 @@ pub async fn libp2p_service(
     Ok(())
 }
 
-fn listen(addr: &Multiaddr, mut transport: Boxed<(PeerId, StreamMuxerBox)>) -> JoinHandle<()> {
-    // Start listening.
-    transport.listen_on(addr.clone()).expect("listen error.");
-    info!("Listening for connections....");
-
-    // In a seperate thread we will log stream events.
-    task::spawn(async move {
-        loop {
-            match transport.next().await.unwrap() {
-                libp2p::core::transport::TransportEvent::NewAddress {
-                    listener_id,
-                    listen_addr,
-                } => info!("NewAddress: {0:?} {1:?}", listener_id, listen_addr),
-                libp2p::core::transport::TransportEvent::AddressExpired {
-                    listener_id,
-                    listen_addr,
-                } => info!("AddressExpired: {0:?} {1:?}", listener_id, listen_addr),
-                libp2p::core::transport::TransportEvent::Incoming {
-                    listener_id,
-                    upgrade,
-                    local_addr,
-                    send_back_addr,
-                } => info!(
-                    "Incoming: {0:?} {1:?} {2:?}",
-                    listener_id, local_addr, send_back_addr
-                ),
-                libp2p::core::transport::TransportEvent::ListenerClosed {
-                    listener_id,
-                    reason,
-                } => info!("ListenerClosed: {0:?} {1:?}", listener_id, reason),
-                libp2p::core::transport::TransportEvent::ListenerError { listener_id, error } => {
-                    info!("ListenerError: {0:?} {1:?}", listener_id, error)
-                }
-            }
-        }
-    })
-}
-
-fn dial(addr: &Multiaddr, mut transport: Boxed<(PeerId, StreamMuxerBox)>) -> JoinHandle<()> {
-    let addr = addr.clone();
-    task::spawn(async move {
-        info!("Dialing listener...");
-        // Dial the listener from a new quic transport.
-        let (peer_id, _connection) = transport
-            .dial(addr)
-            .expect("Can dial peer")
-            .await
-            .expect("Connection ok");
-        info!("Connected to: {peer_id:?}");
-    })
-}
-
-fn create_quic_transport(keypair: identity::Keypair) -> Boxed<(PeerId, StreamMuxerBox)> {
-    // Create a quic transport.
-    let quic_config = quic::Config::new(&keypair);
-    quic::tokio::Transport::new(quic_config)
-        // Not sure why we need to do this conversion to a StreamMuxerBox here, but I found that
-        // it's necessary. Maybe it's because quic handles multiplexing for us already.
-        .map(|(p, c), _| (p, StreamMuxerBox::new(c)))
-        .boxed()
+/// Our network behaviour.
+///
+/// For illustrative purposes, this includes the [`KeepAlive`](behaviour::KeepAlive) behaviour so a continuous sequence of
+/// pings can be observed.
+#[derive(NetworkBehaviour, Default)]
+struct Behaviour {
+    keep_alive: keep_alive::Behaviour,
+    ping: ping::Behaviour,
 }
