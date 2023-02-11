5b4eab341efc9260edff87db12967d3beb93020a
Sam Andreae <contact@samandreae.com>
2023-02-11 22:49:09 +0000

    Update next mod exports

--- aquadoggo/src/test_utils/next/mod.rs
+++ aquadoggo/src/test_utils/next/mod.rs
@@ -2,14 +2,13 @@
 
 mod client;
 mod config;
+mod db;
 mod helpers;
 mod node;
-mod db;
 mod runner;
 
-pub use node::TestNode;
-pub use db::{drop_database, initialize_db, initialize_db_with_url};
 pub use client::{graphql_test_client, shutdown_handle, TestClient};
 pub use config::{TestConfiguration, TEST_CONFIG};
+pub use db::{drop_database, initialize_db, initialize_db_with_url};
 pub use helpers::{build_document, doggo_fields, doggo_schema};
-
+pub use node::TestNode;
