022fb6116e583a6574ea54acf87da269f3a1989c
mycognosist <gnomad@cryptolab.net>
2023-03-07 13:52:32 +0200

    Replace PEM key file with hex-encoded private key

--- aquadoggo/src/network/identity.rs
+++ aquadoggo/src/network/identity.rs
@@ -9,19 +9,16 @@ use std::path::Path;
 use anyhow::Result;
 use libp2p::identity::{ed25519, Keypair};
 use libp2p::PeerId;
-use pem::Pem;
 
-/// Utilities for dealing with a libp2p identity in the form of an ed25519 keypair.
+/// Utilities for dealing with the node identity in the form of an Ed25519 key pair.
 pub trait Identity {
     fn new() -> Self
     where
         Self: Sized;
 
-    fn id(&self) -> PeerId;
+    fn peer_id(&self) -> PeerId;
 
-    fn keypair(&self) -> Keypair;
-
-    fn encode_pem(&self) -> String;
+    fn key_pair(&self) -> Keypair;
 
     fn save(&self, path: &Path) -> Result<()>;
 
@@ -31,88 +28,59 @@ pub trait Identity {
 }
 
 impl Identity for Keypair {
-    /// Generate a random keypair.
+    /// Generate a new Ed25519 key pair.
     fn new() -> Self {
         Keypair::generate_ed25519()
     }
 
-    /// Return the public key of a keypair.
-    fn id(&self) -> PeerId {
+    /// Return the peer ID of a key pair.
+    fn peer_id(&self) -> PeerId {
         PeerId::from(self.public())
     }
 
-    /// Return the keypair.
-    fn keypair(&self) -> Keypair {
+    /// Return the key pair.
+    fn key_pair(&self) -> Keypair {
         self.clone()
     }
 
-    /// Encode the keypair using the PEM (Privacy-Enhanced Mail) format.
-    fn encode_pem(&self) -> String {
-        let pem_data = match self {
-            Keypair::Ed25519(keypair) => {
-                let key = keypair.encode();
-
-                // Write the ASN.1 header id-ed25519 to the buffer
-                let mut buf: Vec<u8> = vec![
-                    0x30, 0x53, 0x02, 0x01, 0x01, 0x30, 0x05, 0x06, 0x03, 0x2B, 0x65, 0x70, 0x04,
-                    0x22, 0x04, 0x20,
-                ];
-                // Extend with secret key
-                buf.extend(key[..32].iter());
-                // Extend with pubkey separator
-                buf.extend([0xA1, 0x23, 0x03, 0x21, 0x00].iter());
-                // Extend with public key
-                buf.extend(key[32..].iter());
-
-                Pem {
-                    tag: "PRIVATE KEY".to_string(),
-                    contents: buf,
-                }
-            }
-        };
-
-        pem::encode(&pem_data)
-    }
-
-    /// Encode the keypair as PEM and write it to the given path.
+    /// Encode the private key as a hex string and save it to the given file path.
     fn save(&self, path: &Path) -> Result<()> {
-        let pem = self.encode_pem();
+        // Retrieve the private key from the key pair
+        let private_key = match self {
+            Keypair::Ed25519(key_pair) => key_pair.secret(),
+        };
+        // Encode the private key
+        let encoded_private_key = hex::encode(private_key);
 
         fs::create_dir_all(path.parent().unwrap())?;
         let mut file = File::create(path)?;
-        file.write_all(pem.as_bytes())?;
+        file.write_all(encoded_private_key.as_bytes())?;
         file.sync_all()?;
 
-        let mut perms = file.metadata()?.permissions();
-        perms.set_mode(0o600);
-        fs::set_permissions(path, perms)?;
+        let mut permissions = file.metadata()?.permissions();
+        permissions.set_mode(0o600);
+        fs::set_permissions(path, permissions)?;
 
         Ok(())
     }
 
-    /// Load a keypair from file at the given path.
+    /// Load a key pair from file at the given path.
     fn load(path: &Path) -> Result<Self>
     where
         Self: Sized,
     {
-        // Read the keypair from file
+        // Read the key pair from file
         let mut file = File::open(path)?;
         let mut contents = String::new();
         file.read_to_string(&mut contents)?;
 
-        let parsed = pem::parse(contents)?;
-        let keypair = match parsed.tag.as_str() {
-            // PEM encoded ed25519 key
-            "PRIVATE KEY" => {
-                // Read the private key - offset 16; 32bytes long
-                let sk_bytes = parsed.contents[16..48].to_vec();
-                let secret = ed25519::SecretKey::from_bytes(sk_bytes)?;
-                // Generate a keypair from the secret key
-                Keypair::Ed25519(secret.into())
-            }
-            _ => panic!("Unsupported key type"),
-        };
+        // Decode the private key
+        let private_key_bytes = hex::decode(contents)?;
+        // Convert the private key bytes into a `SecretKey`
+        let private_key = ed25519::SecretKey::from_bytes(private_key_bytes)?;
+        // Derive a key pair from the private key
+        let key_pair = Keypair::Ed25519(private_key.into());
 
-        Ok(keypair)
+        Ok(key_pair)
     }
 }
