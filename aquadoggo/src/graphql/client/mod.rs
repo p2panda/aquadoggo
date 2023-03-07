fdb5e8a0d524b88d145ab320108ac982ba0559df
Sam Andreae <contact@samandreae.com>
2023-03-07 18:24:00 +0000

    Remove dynamic query module and types

--- aquadoggo/src/graphql/client/mod.rs
+++ aquadoggo/src/graphql/client/mod.rs
@@ -1,8 +1,6 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
 //! API for p2panda clients to publish and query data on this node.
-mod dynamic_query;
-pub mod dynamic_types;
 mod mutation;
 mod query;
 mod static_query;
@@ -11,7 +9,6 @@ mod static_types;
 mod tests;
 mod utils;
 
-pub use dynamic_query::DynamicQuery;
 pub use mutation::ClientMutationRoot;
 pub use query::ClientRoot;
 pub use static_query::StaticQuery;
--- aquadoggo/src/graphql/client/query.rs
+++ aquadoggo/src/graphql/client/query.rs
@@ -3,14 +3,14 @@
 //! Client API root.
 use async_graphql::MergedObject;
 
-use crate::graphql::client::{DynamicQuery, StaticQuery};
+use crate::graphql::client::StaticQuery;
 
 /// Root query object for client api that contains a static and a dynamic part.
 #[derive(MergedObject, Debug)]
-pub struct ClientRoot(StaticQuery, DynamicQuery);
+pub struct ClientRoot(StaticQuery);
 
 impl ClientRoot {
     pub fn new() -> Self {
-        Self(StaticQuery::default(), DynamicQuery::default())
+        Self(StaticQuery::default())
     }
 }
