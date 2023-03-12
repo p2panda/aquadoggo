cc444b37a8c1f0fb7b7cafb2aa18483221331562
Sam Andreae <contact@samandreae.com>
2023-03-12 10:13:08 +0000

    WIP: relation fields

--- aquadoggo/src/graphql/schema_builders/document_field.rs
+++ aquadoggo/src/graphql/schema_builders/document_field.rs
@@ -1,9 +1,13 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
+use async_graphql::Value;
 use async_graphql::dynamic::{Field, FieldFuture, Object, TypeRef};
 use dynamic_graphql::FieldValue;
+use futures::future;
 use p2panda_rs::document::traits::AsDocument;
+use p2panda_rs::operation::OperationValue;
 use p2panda_rs::schema::FieldType;
+use p2panda_rs::storage_provider::traits::DocumentStore;
 
 use crate::db::SqlStore;
 use crate::graphql::utils::{downcast_id_params, get_document_from_params, gql_scalar};
@@ -59,6 +63,33 @@ pub fn build_document_field_schema(
                     // Convert the operation value into a graphql scalar type.
                     //
                     // TODO: Relation fields aren't supported yet, we need recursion.
+                    let value = match value {
+                        // Recurse into single views.
+                        OperationValue::Relation(rel) => {
+                            store.get_document(rel.document_id()).await?
+                        }
+                        OperationValue::PinnedRelation(rel) => {
+                            store.get_document_by_view_id(rel.view_id()).await?
+                        }
+
+                        // Recurse into view lists.
+                        OperationValue::RelationList(rel) => {
+                            let queries = rel
+                                .iter()
+                                .map(|doc_id| store.get_document(doc_id));
+                            Value::List(future::try_join_all(queries).await?)
+                        }
+                        OperationValue::PinnedRelationList(rel) => {
+                            let queries = rel.iter().map(|view_id| {
+                                store.get_document_by_view_id(view_id)
+                            });
+                            Value::List(future::try_join_all(queries).await?)
+                        }
+
+                        // Convert all simple fields to scalar values.
+                        _ => gql_scalar(value),
+                    };
+
                     Ok(Some(FieldValue::value(gql_scalar(value))))
                 }
                 None => Ok(Some(FieldValue::NULL)),
