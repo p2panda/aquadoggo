eab8ccc2ef81c3ed7012c962e74762b7d286dce3
Sam Andreae <contact@samandreae.com>
2023-03-13 08:46:24 +0000

    WIP: still relations...

--- aquadoggo/src/graphql/schema_builders/document_field.rs
+++ aquadoggo/src/graphql/schema_builders/document_field.rs
@@ -58,111 +58,35 @@ pub fn build_document_field_schema(
         let store = ctx.data_unchecked::<SqlStore>();
         let document_id = document_id.clone();
         let document_view_id = document_view_id.clone();
-        let field_type = field_type.clone();
         let name = name.clone();
 
         FieldFuture::new(async move {
-            match field_type {
-                FieldType::Relation(schema_id) => {
-                    let document = get_document_from_params(store, &document_id, &document_view_id).await?;
-                    let field = match document {
-                        Some(document) => document.get(&name).unwrap().clone(),
-                        None => return Ok(FieldValue::NONE),
-                    };
-                    let document_id = match field {
-                        OperationValue::Relation(rel) => rel.document_id().into(),
-                        _ => panic!()
-                    };
-                    match build_relation_field(ctx, schema_id, Some(document_id), None)
-                        .await?
-                    {
-                        Some(value) => Ok(Some(FieldValue::owned_any(value))),
-                        None => Ok(FieldValue::NONE),
-                    }
-                }
-                FieldType::RelationList(schema_id) => todo!(),
-                FieldType::PinnedRelation(schema_id) => {
-                    let document = get_document_from_params(store, &document_id, &document_view_id).await?;
-                    let field = match document {
-                        Some(document) => document.get(&name).unwrap().clone(),
-                        None => return Ok(FieldValue::NONE),
-                    };
-                    let document_view_id = match field {
-                        OperationValue::PinnedRelation(rel) => rel.view_id().into(),
-                        _ => panic!()
-                    };
-                    match build_relation_field(ctx, schema_id, None, Some(document_view_id))
-                        .await?
-                    {
-                        Some(value) => Ok(Some(FieldValue::owned_any(value))),
-                        None => Ok(FieldValue::NONE),
-                    }
-                }
-                FieldType::PinnedRelationList(_) => todo!(),
-                _ => resolve_field(ctx, document_id, document_view_id).await,
+            let (document_id, document_view_id) =
+                if document_id.is_none() && document_view_id.is_none() {
+                    downcast_id_params(&ctx)
+                } else {
+                    (document_id, document_view_id)
+                };
+
+            let document =
+                match get_document_from_params(store, &document_id, &document_view_id).await? {
+                    Some(document) => document,
+                    None => return Ok(FieldValue::NONE),
+                };
+
+            match document.get(&name).unwrap() {
+                OperationValue::Relation(rel) => Ok(Some(FieldValue::owned_any((
+                    Some(DocumentIdScalar::from(rel.document_id())),
+                    None::<DocumentViewIdScalar>,
+                )))),
+                OperationValue::RelationList(rel) => todo!(),
+                OperationValue::PinnedRelation(rel) => Ok(Some(FieldValue::owned_any((
+                    None::<DocumentIdScalar>,
+                    Some(DocumentViewIdScalar::from(rel.view_id())),
+                )))),
+                OperationValue::PinnedRelationList(rel) => todo!(),
+                value => Ok(Some(FieldValue::value(gql_scalar(value)))),
             }
         })
     }))
-}
-
-async fn resolve_field<'a>(
-    ctx: ResolverContext<'a>,
-    document_id: Option<DocumentIdScalar>,
-    document_view_id: Option<DocumentViewIdScalar>,
-) -> Result<Option<FieldValue>, Error> {
-    let store = ctx.data_unchecked::<SqlStore>();
-    let field_name = ctx.field().name();
-
-    let (document_id, document_view_id) = if document_id.is_none() && document_view_id.is_none() {
-        downcast_id_params(&ctx)
-    } else {
-        (document_id, document_view_id)
-    };
-
-    // Get the whole document.
-    //
-    // TODO: This can be optimized with per field SQL queries and data loader.
-    let document = get_document_from_params(store, &document_id, &document_view_id).await?;
-
-    match document {
-        Some(document) => {
-            let value = document
-                .get(field_name)
-                .expect("Only fields defined on the schema can be queried");
-
-            Ok(Some(FieldValue::value(gql_scalar(value))))
-        }
-        None => Ok(FieldValue::NONE),
-    }
-}
-
-async fn build_relation_field<'a>(
-    ctx: ResolverContext<'a>,
-    schema_id: SchemaId,
-    document_id: Option<DocumentIdScalar>,
-    document_view_id: Option<DocumentViewIdScalar>,
-) -> Result<Option<Object>, Error> {
-    let schema_provider = ctx.data_unchecked::<SchemaProvider>();
-    match schema_provider.get(&schema_id).await {
-        Some(schema) => {
-            // Construct the document fields object which will be named `<schema_id>Field`.
-            let schema_field_name = fields_name(&schema.id().to_string());
-            let mut document_schema_fields = Object::new(&schema_field_name);
-
-            // For every field in the schema we create a type with a resolver.
-            //
-            // TODO: We can optimize the field resolution methods later with a data loader.
-            for (name, field_type) in schema.fields() {
-                document_schema_fields = build_document_field_schema(
-                    document_schema_fields,
-                    name.to_string(),
-                    field_type,
-                    document_id.clone(),
-                    document_view_id.clone(),
-                );
-            }
-            Ok(Some(document_schema_fields))
-        }
-        None => todo!(),
-    }
 }
