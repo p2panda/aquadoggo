3c3a4e5df0ac70c03846225428954ab3aeb2eacb
Andreas Dzialocha <x12@adz.garden>
2023-03-17 17:59:48 +0100

    Derive Default for Find struct as well

--- aquadoggo/src/db/stores/query.rs
+++ aquadoggo/src/db/stores/query.rs
@@ -2,14 +2,13 @@
 
 //! This module offers a query API to find one or many p2panda documents, filtered or sorted by
 //! custom parameters. Multiple results are paginated.
-
 use p2panda_rs::document::Document;
 
 use crate::db::errors::QueryError;
 use crate::db::query::{Filter, Order, Pagination};
 use crate::db::SqlStore;
 
-#[derive(Debug, Clone)]
+#[derive(Default, Debug, Clone)]
 pub struct Find {
     filter: Filter,
     order: Order,
