dd369e7b186f1a582fc562364fb89a0780f26b26
Sam Andreae <contact@samandreae.com>
2023-03-28 10:47:45 +0100

    Docs and comments in ordering module

--- aquadoggo/src/graphql/types/ordering.rs
+++ aquadoggo/src/graphql/types/ordering.rs
@@ -6,22 +6,39 @@ use p2panda_rs::schema::Schema;
 
 use crate::graphql::utils::order_by_name;
 
+/// Meta fields by which a collection of documents can be sorted.
+pub const META_ORDER_FIELDS: [&str; 3] = ["OWNER", "DOCUMENT_ID", "DOCUMENT_VIEW_ID"];
+
+/// Possible ordering direction for collection queries.
 #[derive(Enum, Debug)]
 pub enum OrderDirection {
-    Asc,
-    Desc,
+    #[graphql(name = "ASC")]
+    Ascending,
+
+    #[graphql(name = "DESC")]
+    Descending,
 }
 
+/// A constructor for dynamically building an enum type containing all meta and application fields
+/// which a collection of documents can be ordered by.
 pub struct OrderBy;
 
 impl OrderBy {
     pub fn build(schema: &Schema) -> Enum {
-        let mut input_values = Enum::new(order_by_name(schema.id()))
-            .item("OWNER")
-            .item("DOCUMENT_ID")
-            .item("DOCUMENT_VIEW_ID");
-        for (name, _) in schema.fields().iter() {
+        let mut input_values = Enum::new(order_by_name(schema.id()));
+
+        // Add meta fields to ordering enum.
+        //
+        // Meta fields are uppercase formatted strings.
+        for name in META_ORDER_FIELDS {
             input_values = input_values.item(name)
+        }
+
+        // Add document fields to ordering enum.
+        //
+        // Application fields are lowercase formatted strings.
+        for (name, _) in schema.fields().iter() {
+            input_values = input_values.item(name.to_lowercase())
         }
         input_values
     }
