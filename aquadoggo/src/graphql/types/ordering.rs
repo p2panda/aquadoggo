0b0ec389bd50f55b72379a2d27f5d36b53d796f6
Sam Andreae <contact@samandreae.com>
2023-03-28 10:50:26 +0100

    Module string for ordering module

--- aquadoggo/src/graphql/types/ordering.rs
+++ aquadoggo/src/graphql/types/ordering.rs
@@ -1,5 +1,7 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
+//! Types used as inputs when specifying ordering parameters on collection queries. 
+
 use async_graphql::dynamic::Enum;
 use dynamic_graphql::Enum;
 use p2panda_rs::schema::Schema;
