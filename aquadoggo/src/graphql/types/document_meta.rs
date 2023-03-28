43d13c47bcc9a11de04450f7e9717e54ef55fb09
Sam Andreae <contact@samandreae.com>
2023-03-28 12:05:00 +0100

    Add owner field to document meta

--- aquadoggo/src/graphql/types/document_meta.rs
+++ aquadoggo/src/graphql/types/document_meta.rs
@@ -5,17 +5,22 @@ use async_graphql::Error;
 use dynamic_graphql::{FieldValue, SimpleObject};
 use p2panda_rs::document::traits::AsDocument;
 
-use crate::graphql::scalars::{DocumentIdScalar, DocumentViewIdScalar};
+use crate::graphql::scalars::{DocumentIdScalar, DocumentViewIdScalar, PublicKeyScalar};
 use crate::graphql::utils::downcast_document;
 
-/// The meta fields of a document.
+/// Meta fields of a document, contains id and authorship information.
 #[derive(SimpleObject)]
 pub struct DocumentMeta {
+    /// The document id of this document.
     #[graphql(name = "documentId")]
-    pub document_id: DocumentIdScalar,
+    document_id: DocumentIdScalar,
 
+    /// The document view id of this document.
     #[graphql(name = "viewId")]
-    pub view_id: DocumentViewIdScalar,
+    document_view_id: DocumentViewIdScalar,
+
+    /// The public key of the author who first created this document.
+    owner: PublicKeyScalar,
 }
 
 impl DocumentMeta {
@@ -23,20 +28,21 @@ impl DocumentMeta {
     ///
     /// Requires a `ResolverContext` to be passed into the method.
     pub async fn resolve(ctx: ResolverContext<'_>) -> Result<Option<FieldValue<'_>>, Error> {
-        // Parse the bubble up value.
+        // Parse the bubble up parent value.
         let document = downcast_document(&ctx);
 
+        // Extract the document in the case of a single or paginated request.
         let document = match document {
             super::DocumentValue::Single(document) => document,
             super::DocumentValue::Paginated(_, _, document) => document,
         };
 
         // Construct `DocumentMeta` and return it. We defined the document meta
-        // type and already registered it in the schema. It's derived resolvers
-        // will handle field selection.
+        // type and already registered it in the schema.
         let document_meta = Self {
             document_id: document.id().into(),
-            view_id: document.view_id().into(),
+            document_view_id: document.view_id().into(),
+            owner: document.author().to_owned().into(),
         };
 
         Ok(Some(FieldValue::owned_any(document_meta)))
