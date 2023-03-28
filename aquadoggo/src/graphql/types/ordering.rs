6ec7224953681901a86f00055a9d97e13b71c305
Sam Andreae <contact@samandreae.com>
2023-03-28 01:38:05 +0100

    Add all meta field ordering items

--- aquadoggo/src/graphql/types/ordering.rs
+++ aquadoggo/src/graphql/types/ordering.rs
@@ -16,9 +16,7 @@ pub struct OrderBy;
 
 impl OrderBy {
     pub fn build(schema: &Schema) -> Enum {
-        let mut input_values = Enum::new(order_by_name(schema.id())).item("OWNER");
-        let mut input_values = Enum::new(order_by_name(schema.id())).item("DOCUMENT_ID");
-        let mut input_values = Enum::new(order_by_name(schema.id())).item("DOCUMENT_VIEW_ID");
+        let mut input_values = Enum::new(order_by_name(schema.id())).item("OWNER").item("DOCUMENT_ID").item("DOCUMENT_VIEW_ID");
         for (name, _) in schema.fields().iter() {
             input_values = input_values.item(name)
         }
