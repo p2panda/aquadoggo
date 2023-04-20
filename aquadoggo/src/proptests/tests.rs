312a65487a085e81946481ffde0f614cee81ff84
Sam Andreae <contact@samandreae.com>
2023-04-21 00:41:26 +0100

    Refactor test methods

--- aquadoggo/src/proptests/tests.rs
+++ aquadoggo/src/proptests/tests.rs
@@ -2,9 +2,9 @@
 
 use std::collections::HashMap;
 
-use async_graphql::{Name, Response, Value};
+use async_graphql::Response;
 use p2panda_rs::schema::SchemaId;
-use proptest::test_runner::{Config, FailurePersistence, FileFailurePersistence};
+use proptest::test_runner::{Config, FileFailurePersistence};
 use proptest::{prop_compose, proptest, strategy::Just};
 use serde_json::{json, Value as JsonValue};
 
@@ -12,75 +12,6 @@ use crate::proptests::document_strategies::{documents_strategy, DocumentAST};
 use crate::proptests::schema_strategies::{schema_strategy, SchemaAST};
 use crate::proptests::utils::{add_documents_from_ast, add_schemas_from_ast};
 use crate::test_utils::{graphql_test_client, test_runner, TestClient, TestNode};
-
-prop_compose! {
-    fn schema_with_documents_strategy()
-            (schema in schema_strategy())
-            (documents in documents_strategy(schema.clone()), schema in Just(schema))
-            -> (SchemaAST, Vec<DocumentAST>) {
-        (schema, documents)
-    }
-}
-
-proptest! {
-    #![proptest_config(Config {
-        cases: 3, 
-        max_shrink_time: 60000, 
-        max_shrink_iters: 1000, 
-        failure_persistence: Some(Box::new(FileFailurePersistence::WithSource("regressions"))), 
-        .. Config::default()
-      })]
-    #[test]
-    fn test_query((schema_ast, document_ast_collection) in schema_with_documents_strategy()) {
-        // The proptest strategies for generating schema and deriving collections of documents for each injects
-        // the raw AST types into the test. Here we convert these into p2panda `Entries`, `Operations` and `Schema`
-        // which we can then use to populate a store and run queries against.
-
-        // Now we start up a test runner and inject a test node we can populate.
-        test_runner(|mut node: TestNode| async move {
-            // Add all schema to the node.
-            add_schemas_from_ast(&mut node, &schema_ast).await;
-
-            // Add all documents to the node.
-            let mut documents = HashMap::new();
-            for document_ast in document_ast_collection.iter() {
-                add_documents_from_ast(&mut node, &document_ast, &mut documents).await;
-            }
-
-            // Sanity checks
-            assert!(documents.len() > 0);
-
-            for (schema_id, documents) in documents {
-                let expected_total_documents = documents.len();
-
-                let client = graphql_test_client(&node).await;
-
-                let mut query_args = "".to_string();
-
-                loop {
-                    println!("Query for documents of type {0} with query args: {1}", &schema_id, query_args);
-                    let (has_next_page, total_count, end_cursor, documents) = make_query(&client, &schema_id, &query_args).await;
-                    let end_cursor = end_cursor.unwrap(); // We expect every request here to have an end cursor
-                    query_args = format!("(after: \"{0}\")", end_cursor);
-                    let last_document_cursor = documents.last().unwrap().as_object().unwrap().get("cursor").unwrap().as_str().unwrap();
-                    assert_eq!(end_cursor, last_document_cursor);
-                    assert_eq!(total_count, expected_total_documents as i64);
-                    if has_next_page {
-                        assert_eq!(documents.len(), 25)
-                    } else {
-                        assert_eq!(documents.len(), expected_total_documents % 25);
-                        let (has_next_page, total_count, end_cursor, documents) = make_query(&client, &schema_id, &query_args).await;
-                        assert_eq!(end_cursor, None);
-                        assert_eq!(has_next_page, false);
-                        assert_eq!(documents.len(), 0);
-                        assert_eq!(total_count, expected_total_documents as i64);
-                        break;
-                    }
-                }
-            };
-        });
-    }
-}
 
 fn unwrap_response(response: &Response) -> (bool, i64, Option<String>, Vec<JsonValue>) {
     let query_response = response
@@ -145,4 +76,85 @@ async fn make_query(
     let response: Response = response.json().await;
     assert!(response.is_ok());
     unwrap_response(&response)
+}
+
+prop_compose! {
+    fn schema_with_documents_strategy()
+            (schema in schema_strategy())
+            (documents in documents_strategy(schema.clone()), schema in Just(schema))
+            -> (SchemaAST, Vec<DocumentAST>) {
+        (schema, documents)
+    }
+}
+
+async fn paginated_query_meta_fields_only(
+    client: &TestClient,
+    schema_id: &SchemaId,
+    expected_total_documents: usize,
+) {
+    let mut query_args = "".to_string();
+
+    loop {
+        let (has_next_page, total_count, end_cursor, documents) =
+            make_query(&client, &schema_id, &query_args).await;
+        let end_cursor = end_cursor.unwrap(); // We expect every request here to have an end cursor
+        query_args = format!("(after: \"{0}\")", end_cursor);
+        let last_document_cursor = documents
+            .last()
+            .unwrap()
+            .as_object()
+            .unwrap()
+            .get("cursor")
+            .unwrap()
+            .as_str()
+            .unwrap();
+        assert_eq!(end_cursor, last_document_cursor);
+        assert_eq!(total_count, expected_total_documents as i64);
+        if has_next_page {
+            assert_eq!(documents.len(), 25)
+        } else {
+            assert_eq!(documents.len(), expected_total_documents % 25);
+            let (has_next_page, total_count, end_cursor, documents) =
+                make_query(&client, &schema_id, &query_args).await;
+            assert_eq!(end_cursor, None);
+            assert_eq!(has_next_page, false);
+            assert_eq!(documents.len(), 0);
+            assert_eq!(total_count, expected_total_documents as i64);
+            break;
+        }
+    }
+}
+
+proptest! {
+    #![proptest_config(Config {
+        cases: 100,
+        max_shrink_time: 60000,
+        max_shrink_iters: 1000,
+        failure_persistence: Some(Box::new(FileFailurePersistence::WithSource("regressions"))),
+        .. Config::default()
+      })]
+    #[test]
+    fn test_query((schema_ast, document_ast_collection) in schema_with_documents_strategy()) {
+        // The proptest strategies for generating schema and deriving collections of documents for each injects
+        // the raw AST types into the test. Here we convert these into p2panda `Entries`, `Operations` and `Schema`
+        // which we can then use to populate a store and run queries against.
+
+        // Now we start up a test runner and inject a test node we can populate.
+        test_runner(|mut node: TestNode| async move {
+            // Add all schema to the node.
+            add_schemas_from_ast(&mut node, &schema_ast).await;
+
+            // Add all documents to the node.
+            let mut documents = HashMap::new();
+            for document_ast in document_ast_collection.iter() {
+                add_documents_from_ast(&mut node, &document_ast, &mut documents).await;
+            }
+
+            let client = graphql_test_client(&node).await;
+
+            for (schema_id, documents) in documents {
+                paginated_query_meta_fields_only(&client, &schema_id, documents.len()).await;
+            };
+        });
+    }
 }
