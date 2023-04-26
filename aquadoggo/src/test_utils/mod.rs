9c9a31ffb53618b11fd6fe96342ad305b964e775
Andreas Dzialocha <adzialocha@users.noreply.github.com>
2023-04-26 17:53:09 +0200

    Fix race conditions in SQLite database test runner (#345)
    * Do not share SQLite database in memory
    
    * Add entry to CHANGELOG.md

--- aquadoggo/src/test_utils/mod.rs
+++ aquadoggo/src/test_utils/mod.rs
@@ -8,8 +8,8 @@ mod node;
 mod runner;
 
 pub use client::{graphql_test_client, TestClient};
-pub use config::{TestConfiguration, TEST_CONFIG};
-pub use db::{drop_database, initialize_db, initialize_db_with_url};
+pub use config::TestConfiguration;
+pub use db::{drop_database, initialize_db};
 pub use helpers::{build_document, doggo_fields, doggo_schema, schema_from_fields};
 pub use node::{
     add_document, add_schema, add_schema_and_documents, populate_and_materialize,
