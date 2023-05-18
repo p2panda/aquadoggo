0b889f82ba652c96184307c32e73de4c7284a642
Sam Andreae <contact@samandreae.com>
2023-05-18 11:05:41 +0100

    fmt

--- aquadoggo/src/test_utils/mod.rs
+++ aquadoggo/src/test_utils/mod.rs
@@ -15,10 +15,9 @@ pub use client::TestClient;
 pub use config::TestConfiguration;
 pub use db::{drop_database, initialize_db};
 pub use helpers::{build_document, doggo_fields, doggo_schema, schema_from_fields};
-pub use node::{
-    add_document, add_schema, populate_and_materialize,
-    populate_store_config, TestNode,
-};
-#[cfg(feature="graphql")]
+#[cfg(feature = "graphql")]
 pub use node::add_schema_and_documents;
+pub use node::{
+    add_document, add_schema, populate_and_materialize, populate_store_config, TestNode,
+};
 pub use runner::test_runner;
