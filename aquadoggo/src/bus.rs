3c116638c2a8afd9856f665eaffba26e55a711ea
adz <x12@adz.garden>
2023-05-24 10:49:30 +0200

    Add new service message types to enum

--- aquadoggo/src/bus.rs
+++ aquadoggo/src/bus.rs
@@ -1,8 +1,10 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
+use libp2p::PeerId;
 use p2panda_rs::operation::OperationId;
 
 use crate::manager::Sender;
+use crate::replication::SyncMessage;
 
 /// Sender for cross-service communication bus.
 pub type ServiceSender = Sender<ServiceMessage>;
@@ -12,4 +14,13 @@ pub type ServiceSender = Sender<ServiceMessage>;
 pub enum ServiceMessage {
     /// A new operation arrived at the node.
     NewOperation(OperationId),
+
+    /// Node established a bi-directional connection to another node.
+    ConnectionEstablished(PeerId),
+
+    /// Node sent a message to remote node for replication.
+    SentReplicationMessage(PeerId, SyncMessage),
+
+    /// Node received a message from remote node for replication.
+    ReceivedReplicationMessage(PeerId, SyncMessage),
 }
