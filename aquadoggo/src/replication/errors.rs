72140839efdde6e4445db6c703f042abfaa2e138
Sam Andreae <contact@samandreae.com>
2023-05-26 13:05:43 +0100

    Introduce DuplicateSessionRequestError

--- aquadoggo/src/replication/errors.rs
+++ aquadoggo/src/replication/errors.rs
@@ -2,16 +2,15 @@
 
 use thiserror::Error;
 
+use crate::replication::TargetSet;
+
 #[derive(Error, Debug)]
 pub enum ReplicationError {
     #[error("Remote peer requested unsupported replication mode")]
     UnsupportedMode,
 
-    #[error("Tried to initialise duplicate inbound replication session with id {0}")]
-    DuplicateInboundRequest(u64),
-
-    #[error("Tried to initialise duplicate outbound replication session with id {0}")]
-    DuplicateOutboundRequest(u64),
+    #[error("Duplicate session error: {0}")]
+    DuplicateSession(#[from] DuplicateSessionRequestError),
 
     #[error("No session found with id {0}")]
     NoSessionFound(u64),
@@ -27,6 +26,7 @@ pub enum ReplicationError {
 }
 
 #[derive(Error, Debug)]
+#[error(transparent)]
 pub enum IngestError {
     #[error("Schema is not supported")]
     UnsupportedSchema,
@@ -48,4 +48,22 @@ pub enum TargetSetError {
 
     #[error("Target set contains unsorted or duplicate schema ids")]
     UnsortedSchemaIds,
+}
+
+#[derive(Error, Debug)]
+pub enum DuplicateSessionRequestError {
+    #[error("Tried to initialise duplicate inbound replication for already established session with id {0}")]
+    InboundEstablishedSession(u64),
+
+    #[error("Tried to initialise duplicate inbound replication for completed session with id {0}")]
+    InboundDoneSession(u64),
+
+    #[error("Tried to initialise duplicate inbound replication session for existing target set {0:?}")]
+    InboundExistingTargetSet(TargetSet),
+
+    #[error("Tried to initialise duplicate outbound replication session for existing target set {0:?}")]
+    OutboundExistingTargetSet(TargetSet),
+
+    #[error("Tried to initialise duplicate outbound replication session with id {0}")]
+    Outbound(u64),
 }
