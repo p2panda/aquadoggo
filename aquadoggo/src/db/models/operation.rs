2d93eb5d702bef562078d253441ddf8e8b1aa0c1
adz <x12@adz.garden>
2023-07-07 13:35:39 +0200

    Simplify doc-string

--- aquadoggo/src/db/models/operation.rs
+++ aquadoggo/src/db/models/operation.rs
@@ -26,15 +26,10 @@ pub struct OperationRow {
     /// separator.
     pub previous: Option<String>,
 
-    /// Index for the position of this operation once topological sorting of the operation graph
-    /// has been performed.
+    /// Index of this operation once topological sorting of the operation graph has been performed.
     ///
-    /// This is an option as when an operation which is not appended to the tip of a
-    /// document graph is handled then a `reduce` materialization task will be issued and all
-    /// indexes for operations in the effected document re-calculated.
-    ///
-    /// If this value is None we can assume the operation has not been processed yet and we are
-    /// waiting for the reduce task to complete.
+    /// If this value is `None` we can assume the operation has not been processed yet and we are
+    /// waiting for the `reduce` task to complete materialization.
     pub sorted_index: Option<i32>,
 }
 
@@ -111,14 +106,9 @@ pub struct OperationFieldsJoinedRow {
     /// field.
     pub list_index: Option<i32>,
 
-    /// Index for the position of this operation once topological sorting of the operation graph
-    /// has been performed.
+    /// Index of this operation once topological sorting of the operation graph has been performed.
     ///
-    /// This is an option as when an operation which is not appended to the tip of a
-    /// document graph is handled then a `reduce` materialization task will be issued and all
-    /// indexes for operations in the effected document re-calculated.
-    ///
-    /// If this value is None we can assume the operation has not been processed yet and we are
-    /// waiting for the reduce task to complete.
+    /// If this value is `None` we can assume the operation has not been processed yet and we are
+    /// waiting for the `reduce` task to complete materialization.
     pub sorted_index: Option<i32>,
 }
