0ee726afde523e6d146659dbc0953f7bfc04c336
Sam Andreae <contact@samandreae.com>
2023-07-17 12:04:05 +0100

    Refactor swarm construction

--- aquadoggo/src/network/swarm.rs
+++ aquadoggo/src/network/swarm.rs
@@ -3,26 +3,22 @@
 use std::convert::TryInto;
 
 use anyhow::Result;
+use libp2p::core::muxing::StreamMuxerBox;
+use libp2p::core::transport::Boxed;
 use libp2p::identity::Keypair;
-use libp2p::swarm::{Swarm, SwarmBuilder};
+use libp2p::swarm::{behaviour, Swarm, SwarmBuilder};
+use libp2p::PeerId;
 
-use crate::network::behaviour::{RelayBehaviour, ClientBehaviour};
+use crate::network::behaviour::P2pandaBehaviour;
 use crate::network::transport;
 use crate::network::NetworkConfiguration;
 
-pub async fn build_relay_swarm(
+fn build_swarm(
     network_config: &NetworkConfiguration,
-    key_pair: Keypair,
-) -> Result<Swarm<RelayBehaviour>> {
-    let peer_id = key_pair.public().to_peer_id();
-
-    // Prepare QUIC transport
-    let transport =
-        transport::build_relay_transport(&key_pair).await;
-
-    // Instantiate the custom network behaviour
-    let behaviour = RelayBehaviour::new(network_config, key_pair)?;
-
+    transport: Boxed<(PeerId, StreamMuxerBox)>,
+    behaviour: P2pandaBehaviour,
+    peer_id: PeerId,
+) -> Result<Swarm<P2pandaBehaviour>> {
     // Initialise a swarm with QUIC transports and our composed network behaviour
     let swarm = SwarmBuilder::with_tokio_executor(transport, behaviour, peer_id)
         // This method expects a NonZeroU8 as input, hence the try_into conversion
@@ -34,27 +30,24 @@ pub async fn build_relay_swarm(
     Ok(swarm)
 }
 
+pub async fn build_relay_swarm(
+    network_config: &NetworkConfiguration,
+    key_pair: Keypair,
+) -> Result<Swarm<P2pandaBehaviour>> {
+    let peer_id = key_pair.public().to_peer_id();
+    let transport = transport::build_relay_transport(&key_pair).await;
+    let behaviour = P2pandaBehaviour::new(network_config, key_pair, None)?;
+
+    build_swarm(network_config, transport, behaviour, peer_id)
+}
 
 pub async fn build_client_swarm(
     network_config: &NetworkConfiguration,
     key_pair: Keypair,
-) -> Result<Swarm<ClientBehaviour>> {
+) -> Result<Swarm<P2pandaBehaviour>> {
     let peer_id = key_pair.public().to_peer_id();
+    let (transport, relay_client) = transport::build_client_transport(&key_pair).await;
+    let behaviour = P2pandaBehaviour::new(network_config, key_pair, Some(relay_client))?;
 
-    // Prepare QUIC transport
-    let (transport, relay_client) =
-        transport::build_client_transport(&key_pair).await;
-
-    // Instantiate the custom network behaviour
-    let behaviour = ClientBehaviour::new(network_config, key_pair, relay_client)?;
-
-    // Initialise a swarm with QUIC transports and our composed network behaviour
-    let swarm = SwarmBuilder::with_tokio_executor(transport, behaviour, peer_id)
-        // This method expects a NonZeroU8 as input, hence the try_into conversion
-        .dial_concurrency_factor(network_config.dial_concurrency_factor.try_into()?)
-        .per_connection_event_buffer_size(network_config.per_connection_event_buffer_size)
-        .notify_handler_buffer_size(network_config.notify_handler_buffer_size.try_into()?)
-        .build();
-
-    Ok(swarm)
+    build_swarm(network_config, transport, behaviour, peer_id)
 }
