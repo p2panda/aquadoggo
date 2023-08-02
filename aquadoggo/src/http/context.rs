bb87b30731247a1cca97436cd226524ddd7d9293
Sam Andreae <contact@samandreae.com>
2023-08-02 22:01:52 +0100

    Add static file server to `http` service

--- aquadoggo/src/http/context.rs
+++ aquadoggo/src/http/context.rs
@@ -1,16 +1,24 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
+use std::path::PathBuf;
+
 use crate::graphql::GraphQLSchemaManager;
 
 #[derive(Clone)]
 pub struct HttpServiceContext {
     /// Dynamic GraphQL schema manager.
     pub schema: GraphQLSchemaManager,
+
+    /// Path of the directory where blobs should be served from
+    pub blob_dir_path: PathBuf,
 }
 
 impl HttpServiceContext {
     /// Create a new HttpServiceContext.
-    pub fn new(schema: GraphQLSchemaManager) -> Self {
-        Self { schema }
+    pub fn new(schema: GraphQLSchemaManager, blob_dir_path: PathBuf) -> Self {
+        Self {
+            schema,
+            blob_dir_path,
+        }
     }
 }
