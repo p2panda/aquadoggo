a0b8243214a76de3a1edd48d9b9ae6070091fbc8
Sam Andreae <contact@samandreae.com>
2023-08-08 16:38:58 +0100

    Change table creation order in documents migration

--- aquadoggo/migrations/20220510022755_create-documents.sql
+++ aquadoggo/migrations/20220510022755_create-documents.sql
@@ -1,5 +1,11 @@
 -- SPDX-License-Identifier: AGPL-3.0-or-later
 
+CREATE TABLE IF NOT EXISTS document_views (
+    document_view_id            TEXT            NOT NULL UNIQUE,
+    schema_id                   TEXT            NOT NULL,
+    PRIMARY KEY (document_view_id)
+);
+
 CREATE TABLE IF NOT EXISTS document_view_fields (
     document_view_id              TEXT            NOT NULL,
     operation_id                  TEXT            NOT NULL,
@@ -8,12 +14,6 @@ CREATE TABLE IF NOT EXISTS document_view_fields (
 );
 
 CREATE INDEX idx_document_view_fields ON document_view_fields (document_view_id, operation_id, name);
-
-CREATE TABLE IF NOT EXISTS document_views (
-    document_view_id            TEXT            NOT NULL UNIQUE,
-    schema_id                   TEXT            NOT NULL,
-    PRIMARY KEY (document_view_id)
-);
 
 CREATE TABLE IF NOT EXISTS documents (
     document_id                 TEXT            NOT NULL UNIQUE,
