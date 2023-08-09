a71e0b19e7705a2a914a408ca86a16e5acfc32a3
Sam Andreae <contact@samandreae.com>
2023-08-09 13:40:51 +0100

    fmt

--- aquadoggo/src/materializer/service.rs
+++ aquadoggo/src/materializer/service.rs
@@ -8,7 +8,7 @@ use tokio::task;
 use crate::bus::{ServiceMessage, ServiceSender};
 use crate::context::Context;
 use crate::manager::{ServiceReadySender, Shutdown};
-use crate::materializer::tasks::{dependency_task, reduce_task, schema_task, prune_task};
+use crate::materializer::tasks::{dependency_task, prune_task, reduce_task, schema_task};
 use crate::materializer::worker::{Factory, Task, TaskStatus};
 use crate::materializer::TaskInput;
 
