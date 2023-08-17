0df54139ab80f8b227f9ae2866329cac59eead56
Sam Andreae <contact@samandreae.com>
2023-08-17 09:29:43 +0100

    Rework dialer dialing logic

--- aquadoggo/src/network/dialer/behaviour.rs
+++ aquadoggo/src/network/dialer/behaviour.rs
@@ -17,7 +17,7 @@ use libp2p::{Multiaddr, PeerId};
 use log::{debug, warn};
 
 const RETRY_LIMIT: u32 = 8;
-const BACKOFF_SEC_MIN: u64 = 5;
+const BACKOFF_SEC_MIN: u64 = 1;
 const BACKOFF_SEC_MAX: u64 = 60;
 
 /// Events which are sent to the swarm from the dialer.
@@ -51,14 +51,11 @@ pub struct RetryStatus {
 ///    issued from the swarm.
 #[derive(Debug)]
 pub struct Behaviour {
-    /// Queue of known peers we want to make a new dial attempt to.
-    dial: VecDeque<PeerId>,
-
     /// Addresses of known peers.
     address_book: HashMap<PeerId, Multiaddr>,
 
     /// Map of peers whose initial dial attempt failed and we want to re-dial.
-    retry: HashMap<PeerId, RetryStatus>,
+    scheduled_dials: HashMap<PeerId, RetryStatus>,
 
     /// The backoff instance used for scheduling next_dials.
     backoff: Backoff,
@@ -68,11 +65,11 @@ impl Behaviour {
     pub fn new() -> Self {
         let min = Duration::from_secs(BACKOFF_SEC_MIN);
         let max = Duration::from_secs(BACKOFF_SEC_MAX);
-        let backoff = Backoff::new(RETRY_LIMIT, min, max);
+        let mut backoff = Backoff::new(RETRY_LIMIT, min, max);
+        backoff.set_jitter(0.9);
 
         Self {
-            dial: VecDeque::new(),
-            retry: HashMap::new(),
+            scheduled_dials: HashMap::new(),
             address_book: HashMap::new(),
             backoff,
         }
@@ -85,8 +82,8 @@ impl Behaviour {
 
     /// Push a known peer to the dial queue.
     pub fn dial_peer(&mut self, peer_id: PeerId) {
-        if !self.dial.contains(&peer_id) {
-            self.dial.push_back(peer_id);
+        if !self.scheduled_dials.contains_key(&peer_id) {
+            self.schedule_dial(&peer_id);
         }
     }
 
@@ -107,20 +104,20 @@ impl Behaviour {
     ///
     /// If the `REDIAL_LIMIT` is reached then the peer is removed from the map of known peers and
     /// no more redial attempts will occur.
-    fn schedule_retry(&mut self, peer_id: &PeerId) {
-        if let Some(status) = self.retry.get_mut(peer_id) {
-            // If the peer is already in the `retry` map then we check if there is a next backoff
+    fn schedule_dial(&mut self, peer_id: &PeerId) {
+        if let Some(status) = self.scheduled_dials.get_mut(peer_id) {
+            // If the peer is already in the `scheduled_dials` map then we check if there is a next backoff
             // delay based on the current number of retry attempts.
             if let Some(backoff_delay) = self.backoff.next(status.attempts) {
                 // If we haven't reached `RETRY_LIMIT` then we set the `next_dial` time.
                 status.next_dial = Some(Instant::now() + backoff_delay);
             } else {
-                // If we have reached `RETRY_LIMIT` then we remove the peer from the `retry` map.
+                // If we have reached `RETRY_LIMIT` then we remove the peer from the `scheduled_dials` map.
                 debug!("Re-dial attempt limit reached: {peer_id:?}");
-                self.retry.remove(peer_id);
+                self.scheduled_dials.remove(peer_id);
             }
         } else {
-            // If this peer was not in the `retry` map yet we instantiate a new retry status and
+            // If this peer was not in the `scheduled_dials` map yet we instantiate a new retry status and
             // insert the peer.
             let backoff_delay = self
                 .backoff
@@ -131,7 +128,7 @@ impl Behaviour {
                 attempts: 1,
                 next_dial: Some(Instant::now() + backoff_delay),
             };
-            self.retry.insert(peer_id.to_owned(), status);
+            self.scheduled_dials.insert(peer_id.to_owned(), status);
         }
     }
 
@@ -140,7 +137,7 @@ impl Behaviour {
     /// When a connection was successfully established we remove the peer from the retry map as we
     /// no longer need to re-dial them.
     fn on_connection_established(&mut self, peer_id: &PeerId) {
-        if self.retry.remove(peer_id).is_some() {
+        if self.scheduled_dials.remove(peer_id).is_some() {
             debug!("Removed peer from retry queue: {peer_id}");
         };
     }
@@ -150,7 +147,7 @@ impl Behaviour {
     /// We want the schedule the next dialing attempt or drop the peer completely if we have
     /// reached `RETRY_LIMIT`.
     fn on_dial_failed(&mut self, peer_id: &PeerId) {
-        self.schedule_retry(peer_id)
+        self.schedule_dial(peer_id)
     }
 }
 
@@ -222,27 +219,12 @@ impl NetworkBehaviour for Behaviour {
         _cx: &mut Context<'_>,
         _params: &mut impl PollParameters,
     ) -> Poll<ToSwarm<Self::ToSwarm, THandlerInEvent<Self>>> {
-        // First dial the next peer which exist in the `dial` queue.
-        if let Some(peer_id) = self.dial.pop_back() {
-            debug!("Dial: {peer_id}");
-            if let Some(addr) = self.address_book.get(&peer_id) {
-                let opts = DialOpts::peer_id(peer_id.clone())
-                    .addresses(vec![addr.to_owned()])
-                    .condition(PeerCondition::NotDialing)
-                    .condition(PeerCondition::Disconnected)
-                    .build();
-
-                return Poll::Ready(ToSwarm::Dial { opts });
-            };
-        }
-
-        // If there were none we move onto peers we want to re-dial.
         let mut peer_to_retry = None;
         let now = Instant::now();
 
         // Iterate over all peers and take the first one which has `next_dial` set and the
         // scheduled dial time has passed.
-        for (peer_id, status) in &self.retry {
+        for (peer_id, status) in &self.scheduled_dials {
             if let Some(next_dial) = status.next_dial {
                 if next_dial < now {
                     peer_to_retry = Some(peer_id.to_owned());
@@ -253,15 +235,15 @@ impl NetworkBehaviour for Behaviour {
 
         if let Some(peer_id) = peer_to_retry {
             // Unwrap safely as we know the peer exists.
-            let status = self.retry.get_mut(&peer_id).unwrap();
-
-            debug!("Re-dial attempt {} for peer {peer_id}", status.attempts);
+            let status = self.scheduled_dials.get_mut(&peer_id).unwrap();
 
             // Increment the peers dial attempts.
             status.attempts += 1;
 
             // Set the peers `next_dial` value to None. This get's set again if the dial attempt fails.
             status.next_dial = None;
+
+            debug!("Dial attempt {} for peer {peer_id}", status.attempts);
 
             if let Some(addr) = self.address_book.get(&peer_id) {
                 let opts = DialOpts::peer_id(peer_id.clone())
