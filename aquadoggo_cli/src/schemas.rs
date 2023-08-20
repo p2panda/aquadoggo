467193c0a3bb11470563fcd52cd538a4e849da8b
adz <x12@adz.garden>
2023-08-20 13:18:11 +0200

    Remove broken test, code will change anyhow soon

--- aquadoggo_cli/src/schemas.rs
+++ aquadoggo_cli/src/schemas.rs
@@ -1,9 +1,10 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
-use anyhow::{anyhow, Result};
-use p2panda_rs::schema::SchemaId;
 use std::fs::File;
 use std::io::Read;
+
+use anyhow::{anyhow, Result};
+use p2panda_rs::schema::SchemaId;
 use toml::Table;
 
 pub fn read_schema_ids_from_file(file: &mut File) -> Result<Vec<SchemaId>> {
@@ -14,29 +15,4 @@ pub fn read_schema_ids_from_file(file: &mut File) -> Result<Vec<SchemaId>> {
         "No \"supported_schema_ids\" field found config file"
     ))?;
     Ok(value.clone().try_into::<Vec<SchemaId>>()?)
-}
-
-#[cfg(test)]
-mod tests {
-    use std::fs::{create_dir_all, File};
-    use std::io::Write;
-
-    use tempfile::TempDir;
-
-    use super::read_schema_ids_from_file;
-
-    #[test]
-    fn reads_schema_ids_from_file() {
-        let tmp_dir = TempDir::new().unwrap();
-        let mut tmp_path = tmp_dir.path().to_owned();
-        tmp_path.push("schema.toml");
-
-        create_dir_all(tmp_path.parent().unwrap()).unwrap();
-        let mut file = File::create(&tmp_path).unwrap();
-        file.write_all("schemas = [\"plant_0020c11ab63099193a8c8516cc00f88bfc8cdd657b94a99fef2fce86aaaede84f87d\"]".as_bytes()).unwrap();
-
-        let result = read_schema_ids_from_file(&mut file);
-        println!("{result:?}");
-        assert!(result.is_ok());
-    }
 }
