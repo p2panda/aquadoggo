158d64abb971bbb2c1eb3bd506d75edb4726038f
adz <x12@adz.garden>
2023-09-01 12:41:15 +0200

    Add handlers for blob http routes

--- aquadoggo/src/http/api.rs
+++ aquadoggo/src/http/api.rs
@@ -1,21 +1,44 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
+use std::convert::TryInto;
+
+use anyhow::Result;
 use async_graphql::http::{playground_source, GraphQLPlaygroundConfig};
 use async_graphql_axum::{GraphQLRequest, GraphQLResponse};
-use axum::extract::Extension;
+use axum::extract::{Extension, Path};
 use axum::response::{self, IntoResponse};
+use http::StatusCode;
+use p2panda_rs::document::{DocumentId, DocumentViewHash};
 
 use crate::http::context::HttpServiceContext;
 
-/// Handle graphql playground requests at the given path.
+/// Handle GraphQL playground requests at the given path.
 pub async fn handle_graphql_playground(path: &str) -> impl IntoResponse {
     response::Html(playground_source(GraphQLPlaygroundConfig::new(path)))
 }
 
-/// Handle graphql requests.
+/// Handle GraphQL requests.
 pub async fn handle_graphql_query(
     Extension(context): Extension<HttpServiceContext>,
     req: GraphQLRequest,
 ) -> GraphQLResponse {
     context.schema.execute(req.into_inner()).await.into()
+}
+
+pub async fn handle_blob_document(
+    Extension(context): Extension<HttpServiceContext>,
+    Path(document_id): Path<DocumentId>,
+) -> Result<String, StatusCode> {
+    Ok("Hello".into())
+}
+
+pub async fn handle_blob_view(
+    Extension(context): Extension<HttpServiceContext>,
+    // @TODO: Deserialize needs to be implemented for `DocumentViewHash` so we can use it here in
+    // combination with `Path`. Related issue: https://github.com/p2panda/p2panda/issues/514
+    Path((document_id, view_hash)): Path<(DocumentId, String)>,
+) -> Result<String, StatusCode> {
+    let view_hash =
+        DocumentViewHash::new(view_hash.try_into().map_err(|_| StatusCode::BAD_REQUEST)?);
+    Ok("Hello".into())
 }
--- aquadoggo/src/http/service.rs
+++ aquadoggo/src/http/service.rs
@@ -10,20 +10,18 @@ use axum::Router;
 use http::header::CONTENT_TYPE;
 use log::{debug, warn};
 use tower_http::cors::{Any, CorsLayer};
-use tower_http::services::ServeDir;
 
 use crate::bus::ServiceSender;
 use crate::context::Context;
 use crate::graphql::GraphQLSchemaManager;
-use crate::http::api::{handle_graphql_playground, handle_graphql_query};
+use crate::http::api::{
+    handle_blob_document, handle_blob_view, handle_graphql_playground, handle_graphql_query,
+};
 use crate::http::context::HttpServiceContext;
 use crate::manager::{ServiceReadySender, Shutdown};
 
 /// Route to the GraphQL playground
 const GRAPHQL_ROUTE: &str = "/graphql";
-
-/// Route to the blobs static file server
-pub const BLOBS_ROUTE: &str = "/blobs";
 
 /// Build HTTP server with GraphQL API.
 pub fn build_server(http_context: HttpServiceContext) -> Router {
@@ -34,17 +32,15 @@ pub fn build_server(http_context: HttpServiceContext) -> Router {
         .allow_credentials(false)
         .allow_origin(Any);
 
-    // Construct static file server
-    let blob_service = ServeDir::new(http_context.blob_dir_path.clone());
-
     Router::new()
-        // Add blobs static file server
-        .nest_service(BLOBS_ROUTE, blob_service)
         // Add GraphQL routes
         .route(
             GRAPHQL_ROUTE,
             get(|| handle_graphql_playground(GRAPHQL_ROUTE)).post(handle_graphql_query),
         )
+        // Add blob routes
+        .route("/blobs/:document_id", get(handle_blob_document))
+        .route("/blobs/:document_id/:view_hash", get(handle_blob_view))
         // Add middlewares
         .layer(cors)
         // Add shared context
