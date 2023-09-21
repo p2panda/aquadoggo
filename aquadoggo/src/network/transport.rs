ba416eae7d7d34c3a1845f5e1e42b16cd06add1a
Sam Andreae <contact@samandreae.com>
2023-09-21 14:36:35 +0100

    Use libp2p 0.5.3

--- aquadoggo/src/network/transport.rs
+++ aquadoggo/src/network/transport.rs
@@ -6,11 +6,15 @@ use libp2p::core::transport::upgrade::Version;
 use libp2p::core::transport::{Boxed, OrTransport};
 use libp2p::identity::Keypair;
 use libp2p::noise::Config as NoiseConfig;
+use libp2p::quic::tokio::Transport as QuicTransport;
+use libp2p::quic::Config as QuicConfig;
+use libp2p::tcp::tokio::Transport as TcpTransport;
+use libp2p::tcp::Config as TcpConfig;
 use libp2p::yamux::Config as YamuxConfig;
-use libp2p::{relay, tcp, PeerId, Transport};
+use libp2p::{relay, PeerId, Transport};
 
-fn quic_config(key_pair: &Keypair) -> libp2p_quic::Config {
-    let mut config = libp2p_quic::Config::new(key_pair);
+fn quic_config(key_pair: &Keypair) -> QuicConfig {
+    let mut config = QuicConfig::new(key_pair);
     config.support_draft_29 = true;
     config
 }
@@ -22,12 +26,10 @@ pub async fn build_client_transport(
     let (relay_transport, relay_client) = relay::client::new(key_pair.public().to_peer_id());
 
     let transport = {
-        let quic_transport = libp2p_quic::tokio::Transport::new(quic_config(key_pair));
+        let quic_transport = QuicTransport::new(quic_config(key_pair));
 
         let relay_tcp_quic_transport = relay_transport
-            .or_transport(tcp::tokio::Transport::new(
-                tcp::Config::default().port_reuse(true),
-            ))
+            .or_transport(TcpTransport::new(TcpConfig::default().port_reuse(true)))
             .upgrade(Version::V1)
             .authenticate(NoiseConfig::new(key_pair).unwrap())
             .multiplex(YamuxConfig::default())
@@ -46,12 +48,12 @@ pub async fn build_client_transport(
 
 // Build the transport stack to be used by nodes with relay capabilities.
 pub async fn build_relay_transport(key_pair: &Keypair) -> Boxed<(PeerId, StreamMuxerBox)> {
-    let tcp_transport = tcp::tokio::Transport::new(tcp::Config::new().port_reuse(true))
+    let tcp_transport = TcpTransport::new(TcpConfig::new().port_reuse(true))
         .upgrade(Version::V1)
         .authenticate(NoiseConfig::new(key_pair).unwrap())
         .multiplex(YamuxConfig::default());
 
-    let quic_transport = libp2p_quic::tokio::Transport::new(quic_config(key_pair));
+    let quic_transport = QuicTransport::new(quic_config(key_pair));
 
     OrTransport::new(quic_transport, tcp_transport)
         .map(|either_output, _| match either_output {
