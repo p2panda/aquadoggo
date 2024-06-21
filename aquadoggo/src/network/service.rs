90d8ffa482915059f8044df8f14d5809bed87a2e
Sam Andreae <contact@samandreae.com>
2024-06-21 20:23:16 +0100

    Listening on relay circuit no longer sometimes fails

--- aquadoggo/src/network/service.rs
+++ aquadoggo/src/network/service.rs
@@ -264,21 +264,6 @@ pub async fn connect_to_relay(
                 info!("Registered on rendezvous in namespace \"{namespace}\"");
                 rendezvous_registered = true;
             }
-            SwarmEvent::ConnectionClosed { .. } => {
-                // Listening on a relay circuit address opens a connection to the relay node, this
-                // doesn't always succeed first time though, so we catch here when the connection closed
-                // and retry listening.
-                warn!(
-                    "Relay circuit connection closed, re-attempting listening on: {:?}",
-                    circuit_address
-                );
-
-                // After a short wait.
-                tokio::time::sleep(Duration::from_millis(100)).await;
-
-                // Listen again.
-                swarm.listen_on(circuit_address.clone())?;
-            }
             event => debug!("{event:?}"),
         }
 
